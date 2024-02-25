53ac40461a8660333ec56b45115833f1c7760f80
JabbaBC <53377632+JabbaBC@users.noreply.github.com>
2024-02-25 19:07:02 +0300

    Update weighted_bora_canbula.py

--- Week03/weighted_bora_canbula.py
+++ Week03/weighted_bora_canbula.py
@@ -1,5 +1,7 @@
 import random
 
 
-def weighted_srs():
-  return None
+def weighted_srs(data, n, weights, with_replacement=False):
+    if with_replacement:
+        return random.choices(data, weights=weights, k=n)
+    return random.sample(data, n, counts=weights)
