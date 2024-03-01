163ea8e0596ff9fa7c1816bd135765086a9fc666
Extocraftor <51536864+Extocraftor@users.noreply.github.com>
2024-03-01 10:14:57 +0300

    Update weighted_mert_durgun.py

--- Week03/weighted_mert_durgun.py
+++ Week03/weighted_mert_durgun.py
@@ -1,5 +1,5 @@
 import random
 
-def weighted_srs(data, weights, n, with_replacement=True):
-    randomlist = random.choices(data, weights=weights, k=n)
+def weighted_srs(data, n, weights, with_replacement = False):
+    randomlist = random.choices(data, k=n, weights=weights) if weights != None or with_replacement == True else random.sample(data, n)
     return randomlist
