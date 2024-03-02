405f5e4add9fe9cccba5a453daccd19fbad0ed1a
MFarid77 <135022513+MFarid77@users.noreply.github.com>
2024-03-02 22:25:02 +0400

    Update weighted_farid_mammadov.py

--- Week03/weighted_farid_mammadov.py
+++ Week03/weighted_farid_mammadov.py
@@ -6,9 +6,3 @@ def weighted_srs(data, n, weights, with_replacement):
     else:
         total_samples = min(len(data), n)
         return random.sample([random.choices(data, weights=weights, k=1)[0] for _ in range(total_samples)], k=total_samples)
-data_population = ['A', 'B', 'C', 'D', 'E']
-sample_size = 3
-weights_for_population = [10, 1, 1, 1, 1]
-with_replacement_flag = True
-sample = weighted_srs(data_population, sample_size, weights_for_population, with_replacement_flag)
-print(sample)
