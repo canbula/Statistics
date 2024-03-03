fb453838cddacd7e455053ca0c7ed1cb7ea1870f
Hakan Sinal <114807524+TheLastKhan@users.noreply.github.com>
2024-03-03 22:02:52 +0300

    Create weighted_nurettinhakan_sinal.py

---
+++ Week03/weighted_nurettinhakan_sinal.py
@@ -0,0 +1,5 @@
+import random
+def weighted_srs(data, n, weights, with_replacement = False):
+    if weights != None or with_replacement == True:
+        return random.choices(data, k=n, weights=weights)
+    return random.sample(data, n)
