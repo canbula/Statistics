f08898e491acf2c011b0a0b7d6c372a17e53e6be
Hamza <127198357+Hamza-rgn@users.noreply.github.com>
2024-03-04 08:41:41 +0300

    Update weighted_hamza_kızıltoprak.py

--- Week03/weighted_hamza_kızıltoprak.py
+++ Week03/weighted_hamza_kızıltoprak.py
@@ -1,4 +1,4 @@
 import random
 def weighted_srs(data, n, weights, with_replacement = False):
-    sample = random.choices(data, weights=weights, k=n) if with_replacement else random.sample(data, n)
+    sample = random.choices(data, weights=weights, k=n) if with_replacement  or weights != None else random.sample(data, n)
     return sample
