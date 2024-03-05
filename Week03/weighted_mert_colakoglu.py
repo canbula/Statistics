1c095a6c5d023054e9752fc1d4cbd5c89e87b597
Mert Çolakoğlu <130840093+mertm3rt@users.noreply.github.com>
2024-03-05 22:42:33 +0300

    Create weighted_mert_colakoglu.py

---
+++ Week03/weighted_mert_colakoglu.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append(".")
+from Week02 import data
+import random
+def weighted_srs(data: list, n: int, weights=None, with_replacement=False):
+    if with_replacement is not False or weights is not None:
+        return [random.choices(data, k=n, weights=(weights if weights and len(weights) == len(data) else None))]
+    return random.sample(data, min(n, len(data)))
