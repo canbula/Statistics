24337fb33ed5acade34573dcb61fcdabc3d97db7
Hakan Sinal <114807524+TheLastKhan@users.noreply.github.com>
2024-03-05 13:22:15 +0300

    Update weighted_nurettinhakan_sinal.py

--- Week03/weighted_nurettinhakan_sinal.py
+++ Week03/weighted_nurettinhakan_sinal.py
@@ -1,5 +1,6 @@
 import random
 
+
 def weighted_srs(data, n, weights, with_replacement = False):
     if weights != None or with_replacement == True:
         return random.choices(data, k=n, weights=weights)
