50b519a995d39e9e37ffac9af7bb3e768346955b
Özgür Uçar <98189629+ozgurucar@users.noreply.github.com>
2024-03-05 13:22:00 +0300

    Create weighted_ozgur_ucar.py

---
+++ Week03/weighted_ozgur_ucar.py
@@ -0,0 +1,9 @@
+import random
+def weighted_srs(data, n, weights, with_replacement = "False"):
+    if(with_replacement == True or weights != None):
+        sample = random.choices(population=data,k=n,weights=weights)
+    else:
+        sample = random.sample(population=data,k=n)
+    return sample
+
+
