1b9f9df258d67bf0ff439bcbdd335990d0b16b57
erfanghesmati <159188195+erfanghesmati@users.noreply.github.com>
2024-03-05 19:21:59 +0300

    Update weighted_erfan_ghesmati.py

--- Week03/weighted_erfan_ghesmati.py
+++ Week03/weighted_erfan_ghesmati.py
@@ -1,5 +1,6 @@
 import random
-def weighted_srs(data, n, weights, with_replacement=True):
-    probabilities = [w/sum(weights) for w in weights]
+
+def weighted_srs(data, n, weights, with_replacement=False):
+    probabilities = [w / sum(weights) for w in weights]
     sampled_items = random.choices(data, probabilities, k=n) if with_replacement else random.sample(data, n)
     return sampled_items
