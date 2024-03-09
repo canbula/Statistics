8274b967e58ddfb9a31770bfb2192ac8502ac52f
ysfdursun <120355596+ysfdursun@users.noreply.github.com>
2024-03-09 13:36:34 +0300

    Update weighted_ahmetyusuf_dursun.py

--- Week03/weighted_ahmetyusuf_dursun.py
+++ Week03/weighted_ahmetyusuf_dursun.py
@@ -1,6 +1,6 @@
 import random
 def weighted_srs(data, n, weights, with_replacement=False):
-    if with_replacement == True or weights !=None:
+    if with_replacement or weights !=None:
         randomsample=random.choices(data, k=n, weights=weights)
     else:
         randomsample=random.sample(data,n)
