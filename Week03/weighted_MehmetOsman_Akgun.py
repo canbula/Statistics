e0fded4ea9169d68c8cbcd6456d1846a09b5996b
einsteinpikachu <93139894+einsteinpikachu@users.noreply.github.com>
2024-03-10 21:34:41 +0300

    Add files via upload

---
+++ Week03/weighted_MehmetOsman_Akgun.py
@@ -0,0 +1,9 @@
+import random
+def weighted_random_sample(data, n, weights, with_replacement=False):
+    sampled = []
+    while len(sampled) < n:
+        choice = random.choices(data, weights=weights)[0]
+        if with_replacement or choice not in sampled:
+            sampled.append(choice)
+    return sampled
+
