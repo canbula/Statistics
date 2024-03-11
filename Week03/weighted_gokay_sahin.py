398df3583d3eb01220e114430bfcd1a745756b66
Katex <67821908+katex35@users.noreply.github.com>
2024-03-11 10:16:59 +0300

    Update weighted_gokay_sahin.py

--- Week03/weighted_gokay_sahin.py
+++ Week03/weighted_gokay_sahin.py
@@ -1,6 +1,6 @@
 import random
 
-def weighted_simple_random_sampling(data, n, weights, with_replacement=False):
+def weighted_srs(data, n, weights, with_replacement=False):
     if with_replacement or weights is not None:
         return [random.choice(data, n, weights)]
     else:
