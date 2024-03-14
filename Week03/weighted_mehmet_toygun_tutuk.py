9d01cd877134c01bfa3bc8311131fa36b431638b
MehmetToygunTutuk <161248072+MehmetToygunTutuk@users.noreply.github.com>
2024-03-14 23:56:09 +0300

    weighted_mehmet_toygun_tutuk.py

---
+++ Week03/weighted_mehmet_toygun_tutuk.py
@@ -0,0 +1,8 @@
+import random
+
+def weigted_srs(data, n, weights, with_replacement = False):
+  if with_replacement == True or weights != None:
+    sample = random.choice(data, k = n, weights = weights)
+  else:
+    sample = random.sample(data, n)
+  return sample
