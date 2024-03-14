8dea17c18df8f74116263fb219007a0d9ef916f1
einsteinpikachu <93139894+einsteinpikachu@users.noreply.github.com>
2024-03-14 23:18:08 +0300

    Update weighted_MehmetOsman_Akgun.py

--- Week03/weighted_MehmetOsman_Akgun.py
+++ Week03/weighted_MehmetOsman_Akgun.py
@@ -1,10 +1,10 @@
 import random
 
 def weighted_srs(data, n, weights, with_replacement=False):
-    sampled = []
+    sampled = []  # List to hold selected samples
     while len(sampled) < n:
-        choice = random.choices(data, weights=weights)[0]
-        if with_replacement or choice not in sampled:
-            sampled.append(choice)
-    return sampled
+        choice = random.choices(data, weights=weights)[0]  # Randomly select a sample based on weights.
+        if with_replacement or choice not in sampled: # If not sampling with replacement or the chosen sample is not already in the samples list:
+            sampled.append(choice)  # Add the chosen sample to the samples list.
+    return sampled  # Return the list of selected samples
 
