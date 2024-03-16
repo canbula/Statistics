87512983b31451f0b2b0abe8231a36a1e875e61f
YagmurTokdemir <133105596+YagmurTokdemir@users.noreply.github.com>
2024-03-16 23:42:47 +0300

    Update shifted_yagmur_tokdemir.py

--- Week05/shifted_yagmur_tokdemir.py
+++ Week05/shifted_yagmur_tokdemir.py
@@ -1,5 +1,5 @@
 def shifted(data):
     mean_value = mean(data)
     median_value = median(data)
-    difference = ((mean_value - median_value) / median_value) * 100
+    difference = abs((mean_value - median_value) / median_value) * 100
     return difference
