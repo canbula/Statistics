2bf336aae64b9ec89d45e9917607c8709b48c8d8
begumsezer <161290547+begumsezer@users.noreply.github.com>
2024-03-16 14:42:38 +0300

    Update and rename weighted_Begüm_Sezer.py to weighted_begum_sezer.py

--- Week03/weighted_begum_sezer.py
+++ Week03/weighted_begum_sezer.py
@@ -1,4 +1,4 @@
 import random
 
-def weighted_srs(data, n, weights, with_replacement):
+def weighted_srs(data, n, weights, with_replacement= True):
     return random.choices(data, weights=weights, k=n) if with_replacement else random.sample(data, n)
