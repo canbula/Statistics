c036ce5033944134a97cdeffaf03e352468e0a31
Hakan Sinal <114807524+TheLastKhan@users.noreply.github.com>
2024-03-17 14:42:03 +0300

    Update shifted_nurettinhakan_sinal.py

--- Week05/shifted_nurettinhakan_sinal.py
+++ Week05/shifted_nurettinhakan_sinal.py
@@ -5,3 +5,6 @@ def shifted(data):
     median = (sorted_data[n // 2 - 1] + sorted_data[n // 2]) / 2 if n % 2 == 0 else sorted_data[n // 2]
     difference = ((mean - median) / mean) * 100
     return difference
+data = [1, 2, 3, 4, 5]
+result = shifted(data)
+print("Percentage difference between mean and median:", result)
