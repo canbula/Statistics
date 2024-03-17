5afb8c342efa3d3579e71fe8fb28a61f774d4fdf
Hakan Sinal <114807524+TheLastKhan@users.noreply.github.com>
2024-03-17 23:01:31 +0300

    Update shifted_nurettinhakan_sinal.py

--- Week05/shifted_nurettinhakan_sinal.py
+++ Week05/shifted_nurettinhakan_sinal.py
@@ -1,10 +1,7 @@
+import central_tendency as c
+
 def shifted(data):
-    sorted_data = sorted(data)
-    n = len(sorted_data)
-    mean = sum(data) / n
-    median = (sorted_data[n // 2 - 1] + sorted_data[n // 2]) / 2 if n % 2 == 0 else sorted_data[n // 2]
-    difference = ((mean - median) / mean) * 100
-    return difference
-data = [1, 2, 3, 4, 5]
-result = shifted(data)
-print("Percentage difference between mean and median:", result)
+    mean = c.mean(data)
+    median = c.median(data)
+    result = abs((mean - median)/mean) * 100
+    return result
