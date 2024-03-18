2ed911b10bc693c2772d1bef3d474dbc38e03414
YagmurTokdemir <133105596+YagmurTokdemir@users.noreply.github.com>
2024-03-18 10:16:55 +0300

    Update shifted_yagmur_tokdemir.py

--- Week05/shifted_yagmur_tokdemir.py
+++ Week05/shifted_yagmur_tokdemir.py
@@ -1,7 +1,7 @@
 import central_tendency as ct
 
 def shifted(data):
-    mean_value = mean(data)
-    median_value = median(data)
+    mean_value = ct.mean(data)
+    median_value = ct.median(data)
     difference = abs((mean_value - median_value) / median_value) * 100
     return difference
