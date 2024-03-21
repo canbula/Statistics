64d43de287eb1db3f1a5a74b23eb730f0cd844c7
Mert Çolakoğlu <130840093+mertm3rt@users.noreply.github.com>
2024-03-21 20:23:01 +0300

    Update weighted_mert_colakoglu.py

--- Week03/weighted_mert_colakoglu.py
+++ Week03/weighted_mert_colakoglu.py
@@ -1,6 +1,7 @@
 import random
 
-def weighted_srs(data, n, weights, with_replacement=False):
-    if with_replacement is not False or weights is not None:
-        return [random.choices(data, k=n, weights=weights)]
-    return random.sample(data, n)
+def weighted_srs(data, n, weights, with_replacement = False):
+    if with_replacement or weights is not None:
+        return random.choices(data, k = n, weights = weights)
+    else:
+        return random.sample(data, n)
