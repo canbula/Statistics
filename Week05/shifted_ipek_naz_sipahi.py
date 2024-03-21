5398e14d1c02e855ee731fbd663e5636965619b3
İpek Naz Sipahi <142670058+4idoneus@users.noreply.github.com>
2024-03-21 12:44:13 +0300

    UpdateV2 shifted_ipek_naz_sipahi.py

--- Week05/shifted_ipek_naz_sipahi.py
+++ Week05/shifted_ipek_naz_sipahi.py
@@ -3,5 +3,5 @@ import central_tendency
 def shifted(first):
     mean = central_tendency.mean(first)
     median = central_tendency.median(first)
-    percentage = abs(((mean - median) / mean) * 100) 
-    return percentage
+    result = abs(((mean - median) / mean) * 100) 
+    return result
