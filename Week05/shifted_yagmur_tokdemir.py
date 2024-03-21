945033f89e41329bbd8d8a03d6838209f2652990
YagmurTokdemir <133105596+YagmurTokdemir@users.noreply.github.com>
2024-03-21 12:44:08 +0300

    Update shifted_yagmur_tokdemir.py

--- Week05/shifted_yagmur_tokdemir.py
+++ Week05/shifted_yagmur_tokdemir.py
@@ -1,7 +1,5 @@
-import central_tendency as ct
-
 def shifted(data):
-    mean_value = ct.mean(data)
-    median_value = ct.median(data)
-    difference = abs((mean_value - median_value) / median_value) * 100
-    return difference
+    if not data: return "There is no data to calculate the shifted mean and median. Please provide valid data."
+    mean = sum(data) / len(data)
+    median = sorted(data)[len(data) // 2] if len(data) % 2 else sum(sorted(data)[len(data) // 2 - 1:len(data) // 2 + 1]) / 2
+    return abs(((mean - median) / ((mean + median) / 2)) * 100) if median else "Invalid data."
