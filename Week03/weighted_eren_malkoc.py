ef3534c9e73ab4b59d218352ff9914fd13e7a90e
Eren Malkoç <105520516+erenmalkoc@users.noreply.github.com>
2024-03-23 00:57:53 +0300

    Update weighted_eren_malkoc.py

--- Week03/weighted_eren_malkoc.py
+++ Week03/weighted_eren_malkoc.py
@@ -1,7 +1,7 @@
 import random
-
-def weighted_srs(data, n, weights=None, *, with_replacement=False):
+def weighted_srs(data, n, weights, with_replacement=False):
     if with_replacement:
-        return random.choices(data, weights, k=n)
-    choices, actual_weights = list(data), list(weights) if weights else [1] * len(data)
-    return [choices.pop(i) for i in [actual_weights.pop(choices.index(random.choices(choices, actual_weights, k=1)[0])) for _ in range(n)]]
+        return random.choices(data, weights=weights, k=n)
+    else:
+        randomlist = random.sample(data, n)
+    return randomlist
