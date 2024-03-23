4136ddf533b4d5b6bb2350f12816dfa4be5a421a
YagmurTokdemir <133105596+YagmurTokdemir@users.noreply.github.com>
2024-03-23 19:06:04 +0300

    Update shifted_yagmur_tokdemir.py

--- Week05/shifted_yagmur_tokdemir.py
+++ Week05/shifted_yagmur_tokdemir.py
@@ -2,4 +2,4 @@ def shifted(data):
     if not data: return "There is no data to calculate the shifted mean and median. Please provide valid data."
     mean = sum(data) / len(data)
     median = sorted(data)[len(data) // 2] if len(data) % 2 else sum(sorted(data)[len(data) // 2 - 1:len(data) // 2 + 1]) / 2
-    return abs(((mean - median) / ((mean + median) / 2)) * 100) if median else "Invalid data."
+    return abs((mean - median)/mean) * 100 if median else "Invalid data."
