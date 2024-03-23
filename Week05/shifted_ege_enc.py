89bb03a8a0f9ab0c81946a546373df4b6b248ad0
Ege Enç <111919042+egeenc0@users.noreply.github.com>
2024-03-23 21:03:23 +0300

    Update shifted_ege_enc.py

--- Week05/shifted_ege_enc.py
+++ Week05/shifted_ege_enc.py
@@ -7,12 +7,3 @@ def shifted(data):
     
     p_diff = ((mean - median) / mean) * 100
     return p_diff
-
-
-first_data = [10, 20, 30, 40, 50]  #Difference will be 0
-second_data = [25,32.3,46,48] #Difference will not be 0 at this noisy data
-
-first_result = shifted(first_data)
-second_result = shifted(second_data)
-print("Percentage diff:",first_result)
-print("Percentage diff:",second_result)
