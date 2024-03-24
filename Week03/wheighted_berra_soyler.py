2a08b239d45d2e7899c85653ea153d886dc18f59
Berra SÖYLER <118564672+berriesyl@users.noreply.github.com>
2024-03-24 21:47:43 +0300

    Create wheighted_berra_soyler.py

---
+++ Week03/wheighted_berra_soyler.py
@@ -0,0 +1,6 @@
+import random
+def weighted_srs(data, n, weights, with_replacement):
+  if with_replacement == True:
+      return [random.choices(data, weights, k=1)[0] for _ in range(n)]
+  else:
+      return random.sample(data, n)
