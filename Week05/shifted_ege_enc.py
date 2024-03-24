2fda76bdf6024ff0b457054f52fe3428d1c67d1a
Ege Enç <111919042+egeenc0@users.noreply.github.com>
2024-03-24 14:56:13 +0300

    Update shifted_ege_enc.py

--- Week05/shifted_ege_enc.py
+++ Week05/shifted_ege_enc.py
@@ -5,5 +5,5 @@ def shifted(data):
     sorted_data = sorted(data)
     median = (sorted_data[n//2] if n % 2 != 0 else (sorted_data[n//2 - 1] + sorted_data[n//2]) / 2)
     
-    p_diff = ((mean - median) / mean) * 100
+    p_diff = abs((mean - median) / mean) * 100
     return p_diff
