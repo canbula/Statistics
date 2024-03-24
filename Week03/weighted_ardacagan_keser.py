753da23c0d5c7c3d7dbe7b55cfa0ede4c2246a58
ArdaKeser <120564958+ArdaKeser@users.noreply.github.com>
2024-03-24 23:01:41 +0300

    Update weighted_ardacagan_keser.py

--- Week03/weighted_ardacagan_keser.py
+++ Week03/weighted_ardacagan_keser.py
@@ -1,10 +1,6 @@
 import random
 
 def weighted_srs(data, n, weights, with_replacement = False):
-    sample = []
     if with_replacement:
         return random.choices(population = data, weights = weights, k = n)
-    while len(sample) != n:
-        sample += random.choices(population = data, weights = weights, k = 1)
-        sample = list(set(sample))
-    return sample
+    return random.sample(data, min(n, len(data)))
