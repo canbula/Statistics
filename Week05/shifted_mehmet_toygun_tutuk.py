aba84572c7050a6584d1118564a37bb8afc2feef
MehmetToygunTutuk <161248072+MehmetToygunTutuk@users.noreply.github.com>
2024-03-25 00:07:22 +0300

    Update shifted_mehmet_toygun_tutuk.py

--- Week05/shifted_mehmet_toygun_tutuk.py
+++ Week05/shifted_mehmet_toygun_tutuk.py
@@ -1,4 +1,5 @@
 def shifted(data):
-    mean_value = mean(data)
-    median_value = median(data)
+    mean_value = sum(data) / len(data)
+    sorted_data = sorted(data)
+    median_value = sorted_data[len(sorted_data) // 2] if len(data) % 2 != 0 else (sorted_data[len(sorted_data) // 2 - 1] + sorted_data[len(sorted_data) // 2]) / 2
     return abs(mean_value - median_value) / mean_value * 100
