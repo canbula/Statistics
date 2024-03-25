1101961c2775bc59fb12ad0d0686ceee79077e71
Eren Malkoç <105520516+erenmalkoc@users.noreply.github.com>
2024-03-25 09:36:02 +0300

    Update weighted_eren_malkoc.py

--- Week03/weighted_eren_malkoc.py
+++ Week03/weighted_eren_malkoc.py
@@ -1,7 +1,7 @@
 import random
+
 def weighted_srs(data, n, weights, with_replacement=False):
     if with_replacement:
         return random.choices(data, weights=weights, k=n)
-    else:
-        randomlist = random.sample(data, n, counts=weights)
-    return randomlist
+
+    return random.sample(data, n, counts=weights)
