78e6262c1598cddb6080ceec98d435ec13ae5191
Eren Malkoç <105520516+erenmalkoc@users.noreply.github.com>
2024-03-25 12:00:06 +0300

    Create shifted_eren_malkoc.py

---
+++ Week05/shifted_eren_malkoc.py
@@ -0,0 +1,11 @@
+def shifted(data):
+    total = sum(data)
+    length = len(data)
+    sorted_data = sorted(data)
+    mean = total / length
+
+    if length % 2 != 0:
+        median = sorted_data[length // 2]
+
+    else:
+        median = (sorted_data[length // 2 - 1] + sorted_data[length // 2]) / 2
