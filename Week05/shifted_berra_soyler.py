ba66e54990472891d0d431ae5594c0b037a4a8da
Berra SÖYLER <118564672+berriesyl@users.noreply.github.com>
2024-03-26 08:52:50 +0300

    Update shifted_berra_soyler.py

--- Week05/shifted_berra_soyler.py
+++ Week05/shifted_berra_soyler.py
@@ -1,6 +1,6 @@
 def shifted(data):
-    data.sort()
+    sorted_data = data.sort()
     n = len(data)
     mean = sum(data) / n
-    median = (data[n // 2] + data[n // 2 - 1]) / 2 if n % 2 == 0 else data[n // 2]
+    median = data[n//2] if n % 2 == 0 else (data[n//2 - 1] + data[n//2]) / 2
     return abs((mean - median)/median)*100
