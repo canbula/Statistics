fd4d92607d80c1c6bf171e8f533a8cc344956a19
tunakatirci <126062477+tunakatirci@users.noreply.github.com>
2024-03-28 00:52:33 +0300

    Update shifted_tuna_katirci.py

--- Week05/shifted_tuna_katirci.py
+++ Week05/shifted_tuna_katirci.py
@@ -3,4 +3,4 @@ def shifted(data):
   n = len(data)
   mean = sum(data)/n
   median = data[n//2] if n % 2 == 1 else (data[n//2 - 1] + data[n//2])/2
-  return (abs(mean - median)/mean) * 100
+  return abs((mean - median)/mean) * 100
