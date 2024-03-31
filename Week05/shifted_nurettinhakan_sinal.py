d49c38c5b5c4867ef501f1ef0f13f8615aeee272
Hakan Sinal <114807524+TheLastKhan@users.noreply.github.com>
2024-03-31 17:41:45 +0300

    Update shifted_nurettinhakan_sinal.py

--- Week05/shifted_nurettinhakan_sinal.py
+++ Week05/shifted_nurettinhakan_sinal.py
@@ -3,6 +3,5 @@ def shifted(data):
         return 0
     mean = sum(sorted(data)) / len(sorted(data))
     median = (sorted(data)[len(sorted(data)) // 2 - 1] + sorted(data)[len(sorted(data)) // 2]) / 2 if len(sorted(data)) % 2 == 0 else sorted(data)[len(sorted(data)) // 2]
-    
     result = abs((mean - median) / mean) * 100
     return result
