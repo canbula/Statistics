35f98838b66be5156e91376e6f41bb997f9b8a23
Eren Malkoç <105520516+erenmalkoc@users.noreply.github.com>
2024-04-01 15:38:44 +0300

    Update shifted_eren_malkoc.py

--- Week05/shifted_eren_malkoc.py
+++ Week05/shifted_eren_malkoc.py
@@ -1,12 +1,10 @@
 def shifted(data):
-    total = sum(data)
-    length = len(data)
     sorted_data = sorted(data)
-    mean = total / length
-    if length % 2 != 0:
-        median = sorted_data[length // 2]
+    mean = sum(data) / len(data)
+    if len(data) % 2 != 0:
+        median = sorted_data[len(data) // 2]
     else:
-        median = (sorted_data[length // 2 - 1] + sorted_data[length // 2]) / 2
+        median = (sorted_data[len(data) // 2 - 1] + sorted_data[len(data) // 2]) / 2
     difference = abs(mean - median)
     percentage = abs((difference / mean) * 100)
     return percentage
