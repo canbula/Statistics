97a0bc032ebf61acef1c8c218784a793d2d0d181
SelinKrg06 <ayselin79@icloud.com>
2025-03-05 15:15:43 +0300

    Update weighted_AyşeSelin_Kargı.py

--- Week03/weighted_AyşeSelin_Kargı.py
+++ Week03/weighted_AyşeSelin_Kargı.py
@@ -1,5 +1,7 @@
 import random
-
-
-def weighted_srs():
-  return None
+def weight_srs(data , n,weights, with_replacement = False):
+    if with_replacement == True or weights is not None:
+        random_list= random.choices(data , k=n, weights=weights)
+    else:
+        random_list=random.sample(data,n)
+    return random_list
