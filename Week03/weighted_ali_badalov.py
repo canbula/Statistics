baf2a184b4aa95b9587e1d694fbb4f56af8f2e6c
alibedelov <59971076+alibedelov@users.noreply.github.com>
2025-03-15 01:29:52 +0400

    Update weighted_ali_badalov.py
    Type with_replacement fixed.

--- Week03/weighted_ali_badalov.py
+++ Week03/weighted_ali_badalov.py
@@ -1,7 +1,7 @@
 import random
 
-def weighted_srs(data, n, weights, with_replacment = False):
-    if(with_replacment == True or weights != None): #Check if we are using replacement or not
+def weighted_srs(data, n, weights, with_replacement = False):
+    if(with_replacement == True or weights != None): #Check if we are using replacement or not
         return random.choices(data, weights, k=n)
     else:
         return random.sample(data, n)
