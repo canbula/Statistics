82ae6c3bf2c5221d6eab24e1f96de32e34beb9bc
yasemengineer <141280600+yasemengineer@users.noreply.github.com>
2025-03-14 13:29:55 +0300

    Update weighted_yaseminbuse_kirtepe.py

--- Week03/weighted_yaseminbuse_kirtepe.py
+++ Week03/weighted_yaseminbuse_kirtepe.py
@@ -1,5 +1,7 @@
 import random
 
-def weighted_srs(data,n,weights,with_replacement):
-    return random.choices(data, weights=weights, k=n) if with_replacement \
-        else random.sample(data, k=n)
+def weighted_srs(data, n, weights, with_replacement):
+    if with_replacement:
+        return random.choices(data, weights=weights, k=n)
+    else:
+        return random.sample(data, k=n)
