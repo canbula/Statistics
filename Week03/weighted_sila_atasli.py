eb8ad56f10a1bd52a7609b65354e401e0002a23a
Sıla Atasli <160187339+Silaatasli@users.noreply.github.com>
2025-03-16 18:46:21 +0300

    Update weighted_sila_atasli.py

--- Week03/weighted_sila_atasli.py
+++ Week03/weighted_sila_atasli.py
@@ -1,9 +1,8 @@
 import random
 
-def weighted_srs(data, n, weights, with_replacement = False):
+def weighted_srs(data, n, weights=None, with_replacement = False):
     if weights is not None:
          return random.choices(data, k=n, weights=weights)
-    elif with_replacement: 
-         return random.choices(data, n)
-    else:
+    if with_replacement: 
+         return random.choices(data, k=n)
         return random.sample(data, n)
