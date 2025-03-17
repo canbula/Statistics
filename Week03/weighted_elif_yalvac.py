818cd6627a297cb10382b3c6ee6cdb828830bbf7
Elif Yalvaç <59252046+elifyalvac@users.noreply.github.com>
2025-03-17 08:47:51 +0300

    Update weighted_elif_yalvac.py

--- Week03/weighted_elif_yalvac.py
+++ Week03/weighted_elif_yalvac.py
@@ -3,4 +3,4 @@ def weighted_srs(data, n, weights, with_replacement):
     if with_replacement:
         return random.choices(data, weights=weights, k=n)
     else:
-        return random.sample(data, n)
+        return random.sample(data, k=n)
