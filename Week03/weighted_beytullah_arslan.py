1b608ba0750af8a1ecd9bcf35d9a4d1e6a1210ea
beytullaharslannn <151189674+beytullaharslannn@users.noreply.github.com>
2025-03-17 16:12:18 +0300

    Update weighted_beytullah_arslan.py

--- Week03/weighted_beytullah_arslan.py
+++ Week03/weighted_beytullah_arslan.py
@@ -1,6 +1,6 @@
 import random
 def weighted_srs(data,n,weights,with_replacement=False):
   if with_replacement==True:
-    return random.choices(data,weights=weights,t=n)
+    return random.choices(data,n,weights=weights)
 
   return random.sample(data,n,counts=weights)
