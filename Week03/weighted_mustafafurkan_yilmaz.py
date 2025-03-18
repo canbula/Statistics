37ce4f92155420211920698b9073cc34c5648eb9
Mustafa Furkan Yılmaz <mustafafurkanyilmaz16@gmail.com>
2025-03-18 16:29:20 +0300

    Update weighted_mustafafurkan_yilmaz.py

--- Week03/weighted_mustafafurkan_yilmaz.py
+++ Week03/weighted_mustafafurkan_yilmaz.py
@@ -1,6 +1,6 @@
 import random
 
-def weighted_srs(data, n, weights = None, with_replacement = False):
+def weighted_srs(data, n, weights, with_replacement = False):
     if weights == None and with_replacement == False:
         return random.sample(data, n)
     else:
