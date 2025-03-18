5721e1f72ffa6c6d89007d6a417d0a796873c480
gulfemsolak <158780850+gulfemsolak@users.noreply.github.com>
2025-03-18 18:44:19 +0300

    Update weighted_gulfem_solak.py

--- Week03/weighted_gülfem_solak.py
+++ Week03/weighted_gülfem_solak.py
@@ -1,10 +1,10 @@
 import random
+def weighted_srs(data,n,weights,with_replacement=True):
+    if with_replacement:
+        return random.choices(data,k=n,weights=weights)
+    else:
+        return random.sample(data, n)
 data = ["Pink","Blue","Purple","White","Black"]
 n = 3
 weights=[30,25,15,20,10]
-with_replacement=True
-
-def weighted_srs(data,n,weights,with_replacement):
-    return random.choices(data,weights=weights,k=n)
-
 print(weighted_srs(data,n,weights,True))
