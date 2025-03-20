3bfacedbb662b92b411438e58b793867e439decd
Fevzi Bağrıaçık <124450541+fevzibagriacik@users.noreply.github.com>
2025-03-20 15:59:36 +0300

    Update weighted_fevzi_bagriacik.py

--- Week03/weighted_fevzi_bagriacik.py
+++ Week03/weighted_fevzi_bagriacik.py
@@ -1,9 +1,4 @@
 import random 
 
 def weighted_srs(population, weight, k):
-    ##population = ["Ali", "Ayşe", "Fatma", "Kazım", "Mehmet"]
-    ##weight = [20, 20, 20, 20, 20]
-    ##k = 3
     return random.choices(population, weight, k=k)
-
-print(weighted_srs(["Ali", "Ayse", "Fatma", "Kazım", "Mehmet"], [20, 20, 20, 20, 20], 3))
