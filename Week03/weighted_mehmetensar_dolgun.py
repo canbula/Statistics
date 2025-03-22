f6b6dadf101d4eaa6ff8a4226947b711a440762b
mehmetensardolgun <mehmetensardolgun@gmail.com>
2025-03-23 02:36:07 +0300

    Delete Week03/weighted_mehmetensar_dolgun.py

--- Week03/weighted_mehmetensar_dolgun.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def weighted_srs(data, n, weights, with_replacement=True):
-  
-     return random.sample(data,n) if with_replacement==False and weights==None  else return random.choices(data,k=n,weights=weights)
-    
-
-    
-   
