782bdf24069bad378ba7f4dc0b311c84ab877db6
mehmetensardolgun <mehmetensardolgun@gmail.com>
2025-03-23 02:33:56 +0300

    Update weighted_mehmetensar_dolgun.py

--- Week03/weighted_mehmetensar_dolgun.py
+++ Week03/weighted_mehmetensar_dolgun.py
@@ -1,8 +1,9 @@
 import random
 
-def weighted_srs(data, n, weights, with_replacement=False):
-  if with_replacement==True or weights!=None:
-    sample=random.choices(data,k=n,weights=weights)
-  else:
-    sample=random.sample(data,n)
-  return sample  
+def weighted_srs(data, n, weights, with_replacement=True):
+  
+     return random.sample(data,n) if with_replacement==False and weights==None  else return random.choices(data,k=n,weights=weights)
+    
+
+    
+   
