07af6c7f50cfa231d79beb6410f5acab3b282c5f
EgeKrts120 <220316064@ogr.cbu.edu.tr>
2025-03-24 23:28:06 +0300

    Update weighted_ege_karatas.py

--- Week03/weighted_ege_karatas.py
+++ Week03/weighted_ege_karatas.py
@@ -1,8 +1,9 @@
 import random 
 
-def weigthted_srs(data ,x , weights, with_replacement = False):
-  if with_replacement == True  or  weights =! None:
-     randomlist = random.choices( data , k = x , weights =  wights)
-  else:
-    randomlist =random.sample( data, x)
-return randomlist
+def weighted_srs(data, x, weights=None, with_replacement=False):
+    if with_replacement or weights is not None:
+        random_list = random.choices(data, k=x, weights=weights)
+    else:
+        random_list = random.sample(data, x)
+    return random_list
+
