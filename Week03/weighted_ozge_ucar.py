5cc8c4a2ac6c248961a5109ad1e694dc12dbc242
özge <162044805+ozgeucr@users.noreply.github.com>
2025-03-26 09:00:47 +0300

    Update weighted_ozge_ucar.py

--- Week03/weighted_ozge_ucar.py
+++ Week03/weighted_ozge_ucar.py
@@ -1,7 +1,7 @@
 import random
 def weight_srs(data , n,weights, with_replacement = False):
     if with_replacement == True or weights != None:
-        random_list= random.choices(data , m=n, weights=weights)
+        random_list= random.choices(data , m=n, weights=weights) #i prefer choices method because of sample took just two variable
     else:
         random_list=random.sample(data,n)
     return random_list
