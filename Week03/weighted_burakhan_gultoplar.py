57e48f046d6c7d6e5a76e3718fe1fe45a21602cb
Burakhan7 <141933959+Burakhan7@users.noreply.github.com>
2025-03-28 18:48:12 +0300

    Update weighted_burakhan_gultoplar.py

--- Week03/weighted_burakhan_gultoplar.py
+++ Week03/weighted_burakhan_gultoplar.py
@@ -1,8 +1,8 @@
 import random
 
 def weighted_srs(data, n, weights, with_replacement):
-    if(with_replacement): return random.choices(data, weights=weights, k=n )       
-    else:     return random.sample(data, n)
-population=['lawyer', 'engineer', 'student', 'driver', 'firefighter']
-weights=[0.2, 0.5, 0.9, 0.7, 0.1]
-sample=weighted_srs(population, 3, weights, True)
+    if with_replacement == True or weights!=None:
+         random.choices(data, k=n , weights=weights )       
+    else:    
+         random.sample(data, n)
+return sample
