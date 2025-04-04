ab854aeba7fce296cbe123c2feabdf39fbe4d8c6
SlainST <serhatkeremtufan@gmail.com>
2025-04-04 23:09:05 +0300

    Update weighted_serhat_tufan.py
    turned n=3 too

--- Week03/weighted_serhat_tufan.py
+++ Week03/weighted_serhat_tufan.py
@@ -1,5 +1,5 @@
 import random
-def weighted_srs(data=data,n=n,weights=weights,with_replacement=True):
+def weighted_srs(data=data,n=3,weights=weights,with_replacement=True):
     if(with_replacement):
         return random.choices(population=data,weights=weights,k=n)
     else:
