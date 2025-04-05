79bf568bd53d5c896cf9b98e983e4eb9a5654575
Mert Yüksel <148434218+MertYksl03@users.noreply.github.com>
2025-04-05 18:03:33 +0300

    Create shifted_mert_yuksel.py

---
+++ Week05/shifted_mert_yuksel.py
@@ -0,0 +1,14 @@
+def shifted(x):
+    
+    n = len(x)
+
+    x_bar = sum(x) / n
+
+    if n % 2 == 1:
+        x_tilde = x[int((n + 1) / 2 - 1)]
+    else:
+        x_tilde = (x[int((n / 2) - 1)] + x[int(n / 2)]) / 2
+
+    diff = abs(x_bar - x_tilde)
+
+    return int(diff * 100) 
