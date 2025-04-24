68cb58ad48e5c1c667c1241c33e3cb8960c284b3
Mustafa Kılıç <135388886+kilic-mustafa@users.noreply.github.com>
2025-04-24 17:52:22 +0300

    Update weighted_mustafa_kilic.py

--- Week03/weighted_mustafa_kilic.py
+++ Week03/weighted_mustafa_kilic.py
@@ -1,6 +1,6 @@
 import random
 
 def weighted_srs(data, n, weights, with_replacement=False):
-    if with_replacement:
+    if with_replacement == True or weights != None:
         return random.choices(data, weights=weights, k=n)
     return random.sample(data, n)
