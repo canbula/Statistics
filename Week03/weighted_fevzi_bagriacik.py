19c0f831f509c69fc34187fcd7cf2bb90dd101e9
Fevzi Bağrıaçık <124450541+fevzibagriacik@users.noreply.github.com>
2025-04-26 12:54:59 +0300

    Update weighted_fevzi_bagriacik.py

--- Week03/weighted_fevzi_bagriacik.py
+++ Week03/weighted_fevzi_bagriacik.py
@@ -1,4 +1,6 @@
 import random 
 
-def weighted_srs(population, weight, k):
-    return random.choices(population, weight, k=k)
+def weighted_srs(population, n, weight, with_replacement=False):
+    if with_replacement == True or weight != None:
+        return random.choices(population, weight=weight, k=n)
+    return random.sample(population, n)
