4260485830c5750c107548348616c810abded338
Ahmet alp Özdemir <109877223+akoalp@users.noreply.github.com>
2025-04-30 00:38:25 +0300

    Update shifted_alp_ozdemir.py
    I saw test results and take action

--- Week05/shifted_alp_ozdemir.py
+++ Week05/shifted_alp_ozdemir.py
@@ -1,10 +1,9 @@
 def shifted(data):
+    data = [abs(value) for value in data]
     n = len(data)
     mean = int(sum(data) / n)
     if n % 2 == 1:
         median = data[int(n + 1 / 2 - 1)]
     median = int(data[int((n / 2) - 1)] + data[int(n / 2)] / 2)
-    if mean == 0:
-        return 0
     result = int(abs((mean - median) / mean) * 100)
     return result
