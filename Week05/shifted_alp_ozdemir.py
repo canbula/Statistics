c3e5cd6ddb41e43def76f12cb3b810bbc91c9aa6
Ahmet alp Özdemir <109877223+akoalp@users.noreply.github.com>
2025-04-30 00:27:44 +0300

    Update shifted_alp_ozdemir.py

--- Week05/shifted_alp_ozdemir.py
+++ Week05/shifted_alp_ozdemir.py
@@ -4,5 +4,7 @@ def shifted(data):
     if n % 2 == 1:
         median = data[int(n + 1 / 2 - 1)]
     median = int(data[int((n / 2) - 1)] + data[int(n / 2)] / 2)
+     if mean == 0:
+        return 0
     result = int(abs((mean - median) / mean) * 100)
     return result
