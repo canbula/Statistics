de33ba040bf5db588669a3d841b70ae04b8610c9
Ahmet alp Özdemir <109877223+akoalp@users.noreply.github.com>
2025-04-29 14:29:25 +0300

    Update shifted_alp_ozdemir.py

--- Week05/shifted_alp_ozdemir.py
+++ Week05/shifted_alp_ozdemir.py
@@ -4,6 +4,6 @@ def shifted(data):
     
     mean = statistics.mean(data)
     median = statistics.median(data)
-    shifted_ratio = int((abs(mean - median) / mean)  * 100)
+    shifted_ratio = int((abs(mean - median) / abs(mean))  * 100)
     
     return shifted_ratio
