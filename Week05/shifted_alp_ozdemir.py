bc1421c681775101b93515a9d6d82f39ee0fc3cc
Ahmet alp Özdemir <109877223+akoalp@users.noreply.github.com>
2025-04-29 14:16:33 +0300

    Create shifted_alp_ozdemir.py
    First commit

---
+++ Week05/shifted_alp_ozdemir.py
@@ -0,0 +1,11 @@
+import statistics
+
+def shifted(data):
+    
+    mean = statistics.mean(data)
+    median = statistics.median(data)
+    shifted_ratio = int((abs(mean - median) / mean)  * 100)
+    
+    return shifted_ratio
+
+
