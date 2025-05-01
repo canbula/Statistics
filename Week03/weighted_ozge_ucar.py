721285a477cffd5d500bc64930e7c6b043e1a0a4
özge <162044805+ozgeucr@users.noreply.github.com>
2025-05-01 15:40:31 +0300

    Update weighted_ozge_ucar.py
    I added the test scenario(data set )

--- Week03/weighted_ozge_ucar.py
+++ Week03/weighted_ozge_ucar.py
@@ -1,7 +1,10 @@
 import random
+data = ['a','b','c','d','e']
+weights=[0.1, 0.2, 0.7, 0.9, 0.3]
 def weight_srs(data , n,weights, with_replacement = False):
     if with_replacement == True or weights != None:
-        random_list= random.choices(data , m=n, weights=weights) #i prefer choices method because of sample took just two variable
+        random_list= random.choices(data , k=n, weights=weights)
     else:
         random_list=random.sample(data,n)
     return random_list
+print(weight_srs(data,3,weights,with_replacement=True))
