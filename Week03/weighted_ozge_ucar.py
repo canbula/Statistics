9d5f3065b7364ac70a1fbb889bc8b10292d1c4ea
özge <162044805+ozgeucr@users.noreply.github.com>
2025-05-03 13:54:17 +0300

    Update weighted_ozge_ucar.py
    I added the comments of the program

--- Week03/weighted_ozge_ucar.py
+++ Week03/weighted_ozge_ucar.py
@@ -1,10 +1,8 @@
 import random
-data = ['a','b','c','d','e']
-weights=[0.1, 0.2, 0.7, 0.9, 0.3]
-def weight_srs(data , n,weights, with_replacement = False):
+def weight_srs(data , n,weights, with_replacement = False):#i create a function which named weight_srs take 4 parameter
     if with_replacement == True or weights != None:
-        random_list= random.choices(data , k=n, weights=weights)
+        random_list= random.choices(data , k=n, weights=weights)#random_list variable hold a random choices from data set with weights
     else:
-        random_list=random.sample(data,n)
-    return random_list
-print(weight_srs(data,3,weights,with_replacement=True))
+        random_list=random.sample(data,n)#if there is no weights then random_list variable hold just random elenments of data set
+    return random_list#the function weight_srs return the random_list
+print(weight_srs(data,3,weights,with_replacement=True))#i called the function in a print operation
