8bb577ea137bb8f0c6d6362ad3cd6189c8cc94f3
busedemirbass <busedemirbas03@gmail.com>
2025-05-08 13:31:39 +0300

    Update weighted_buse_demirbas.py

--- Week03/weighted_buse_demirbas.py
+++ Week03/weighted_buse_demirbas.py
@@ -1,6 +1,6 @@
 import random
-def weighted_srs(data , n , weights , with_replacement=True):
-    if with_replacement == True and weights is not None:
-      return  random.choices (data , weights = weights , k=n) 
+def weighted_srs(data , n , weights , with_replacement = False):
+    if with_replacement == True and wight is not none:
+        return.choices(data , n , weights = weights)
     else:
-       return random.sample(data , n)
+        return random.sample(data , n )
