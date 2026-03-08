129e9562f19f0b272997a446073a7cca0f40fac4
Semi <69583229+Semi-2005@users.noreply.github.com>
2026-03-08 18:34:04 +0300

    Add weighted sampling function for Semi Kazar in Week02

---
+++ Week02/weighted_semi_kazar.py
@@ -0,0 +1,8 @@
+import random
+def weighted_srs(data, n, weights, with_replacement = False):
+    if with_replacement != True or weights is None:
+        sample = random.sample(data, n)
+    else:
+        sample = random.choices(data, weights=weights, k=n)
+
+    return sample
