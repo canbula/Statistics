19c2f4869e3fd6aad560765672192d60ff5a712b
ardasevinc123 <ardasvnc1@gmail.com>
2026-03-10 17:13:29 +0300

    Create weighted_arda_sevinc.py

---
+++ Week02/weighted_arda_sevinc.py
@@ -0,0 +1,10 @@
+import random
+def weighted_srs(data,n,weights,with_replacement):
+    res=[]
+    d=list(data); w=list(weights)
+    for _ in range(n):
+        i=random.choices(range(len(d)),weights=w,k=1)[0]
+        res.append(d[i])
+        if not with_replacement:
+            d.pop(i); w.pop(i)
+    return res
