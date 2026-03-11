04464694eacf7381c0c1cfa2d265b74eb0256b75
Khazar Abdullayev <emperorkhazar1299@gmail.com>
2026-03-11 19:49:56 +0300

    Add files via upload

---
+++ Week02/weighted_khazar_abdullayev.py
@@ -0,0 +1,9 @@
+import random
+
+def weighted_srs(data, n, weights, with_replacement):
+    total_weight = sum(weights)
+    probabilities = [w / total_weight for w in weights]
+    
+    sample = random.choices(data, weights=probabilities, k=n, replace=with_replacement)
+    
+    return sample
