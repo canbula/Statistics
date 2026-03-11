edacd58b8a5c95669d5697e66f9ed2184692307f
Khazar Abdullayev <emperorkhazar1299@gmail.com>
2026-03-11 19:55:24 +0300

    Remove unnecessary blank lines in weighted_srs function

--- Week02/weighted_khazar_abdullayev.py
+++ Week02/weighted_khazar_abdullayev.py
@@ -3,7 +3,5 @@ import random
 def weighted_srs(data, n, weights, with_replacement):
     total_weight = sum(weights)
     probabilities = [w / total_weight for w in weights]
-    
     sample = random.choices(data, weights=probabilities, k=n, replace=with_replacement)
-    
     return sample
