40b91fa9579fc7bbcf9fe8ba6753c5a0be7dee06
yiğit boncuk <boncukyigit5@gmail.com>
2026-03-12 16:55:05 +0300

    Add weighted sampling function with options

---
+++ Week02/weighted_yigit_boncuk.py
@@ -0,0 +1,12 @@
+import random
+
+def weighted_srs(data, n, weights, with_replacement):
+    if with_replacement:
+        return random.choices(data, weights=weights, k=n) 
+    result, d_copy, w_copy = [], list(data), list(weights)
+    for _ in range(n):
+        pick = random.choices(d_copy, weights=w_copy, k=1)[0] 
+        idx = d_copy.index(pick) 
+        result.append(d_copy.pop(idx)) 
+        w_copy.pop(idx) 
+    return result
