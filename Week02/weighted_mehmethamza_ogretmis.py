a713eec887f522fbf048130de316c3b3f9234a09
HAMZA\hamza <hamzaogretmis@gmail.com>
2026-03-12 20:30:47 +0300

    Week02 homework

---
+++ Week02/weighted_mehmethamza_ogretmis.py
@@ -0,0 +1,6 @@
+import random
+
+def weighted_srs(data, n, weights, with_replacement=False):
+    if with_replacement or weights:
+        return random.choices(data, weights=weights, k=n)
+    return random.sample(data, n)
