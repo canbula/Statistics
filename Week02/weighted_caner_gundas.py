11dbe701fad720afd69eb6bcb05c17d4f28749f3
canergundas <230316044@ogr.cbu.edu.tr>
2026-03-12 20:59:27 +0300

    Create weighted_caner_gundas.py

---
+++ Week02/weighted_caner_gundas.py
@@ -0,0 +1,10 @@
+import random
+
+def weighted_srs(data, n, weights, with_replacement):
+    if with_replacement: return random.choices(data, weights=weights, k=n)
+    res, d, w = [], list(data), list(weights)
+    for _ in range(n):
+        idx = random.choices(range(len(d)), weights=w)[0]
+        res.append(d.pop(idx))
+        w.pop(idx)
+    return res
