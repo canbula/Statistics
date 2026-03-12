0beb392fa556886d5bb0cdd95965dd296456094e
Khazar Abdullayev <emperorkhazar1299@gmail.com>
2026-03-12 23:14:01 +0300

    Refactor weighted_srs function for improved sampling
    Refactor weighted sampling function to use a loop for selection and added keyword argument for replacement.

--- Week02/weighted_khazar_abdullayev.py
+++ Week02/weighted_khazar_abdullayev.py
@@ -1,7 +1,10 @@
 import random
-
-def weighted_srs(data, n, weights, with_replacement):
-    total_weight = sum(weights)
-    probabilities = [w / total_weight for w in weights]
-    sample = random.choices(data, weights=probabilities, k=n, replace=with_replacement)
+def weighted_srs(data, n, weights, *, with_replacement=False):
+    w = [1] * len(data) if not weights else weights[:]
+    sample = []
+    for _ in range(n):
+        r = random.uniform(0, sum(w))
+        for i in range(len(data)):
+            r -= w[i]
+            if r <= 0: sample.append(data[i]); w[i] = w[i] if with_replacement else 0; break
     return sample
