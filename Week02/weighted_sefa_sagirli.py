f7580044903c83662f95d94e39759079743568d7
Sefa Sağırlı <143504944+sefas24@users.noreply.github.com>
2026-03-13 01:40:01 +0300

    Week02 HW - Implement weighted sampling function in Python

---
+++ Week02/weighted_sefa_sagirli.py
@@ -0,0 +1,10 @@
+import random 
+def weighted_srs(data, n, weigths, with_replacement):
+  if with_replacement == True: return random.choices(data, weights=weights, k=n)
+    copy = list(weights)
+result = []
+for i in range(n):
+  chosen_index = random.choices(range(len(data)), weigths = copy, k=1)[0] 
+result.append(data[chosen_index])
+copy[chosen_index] = 0
+return result
