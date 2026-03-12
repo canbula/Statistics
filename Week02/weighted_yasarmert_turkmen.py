966b1b39c77feee589c1a2814a182a985049323a
YasarMert <95618275+YasarMert@users.noreply.github.com>
2026-03-12 17:04:22 +0300

    Created weighted_yasarmert_turkmen.py for Week2

---
+++ Week02/weighted_yasarmert_turkmen.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append(".")
+from Week02 import data
+import random
+def weighted_srs(data, n, weights, with_replacement):
+    if with_replacement:
+        return random.choices(data, weights=weights, k=n)
+    else:
+        return random.sample(data,n)
+
