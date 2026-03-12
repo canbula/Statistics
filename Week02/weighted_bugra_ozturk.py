aecd327682fc50b9dff0cc7638baabf72c751456
Buğra Öztürk <ozturkbugra684@gmail.com>
2026-03-12 23:57:45 +0300

    Add weighted_srs function for sampling data
    Implement weighted simple random sampling function.

---
+++ Week02/weighted_bugra_ozturk.py
@@ -0,0 +1,13 @@
+import random
+
+def weighted_srs(data, n, weights, with_replacement):
+    if with_replacement:
+        return random.choices(data, weights=weights, k=n)
+    
+    result = []
+    temp_data, temp_weights = list(data), list(weights)
+    for _ in range(n):
+        choice = random.choices(range(len(temp_data)), weights=temp_weights, k=1)[0]
+        result.append(temp_data.pop(choice))
+        temp_weights.pop(choice)
+    return result
