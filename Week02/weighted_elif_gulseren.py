b7cb1c5cebdd9127dc0519a8c9b35dd5ce08324e
elifgulseren <elifgulseren88@gmail.com>
2026-03-12 17:53:00 +0300

    Create weighted_elif_gulseren.py

---
+++ Week02/weighted_elif_gulseren.py
@@ -0,0 +1,10 @@
+import random
+def weighted_srs(data, n, weights, with_replacement):
+    if with_replacement:
+        return random.choices(data, weights=weights, k=n)
+    res, w = [], list(weights)
+    for _ in range(n):
+        i = random.choices(range(len(data)), weights=w, k=1)[0]
+        res.append(data[i])
+        w[i] = 0
+    return res
