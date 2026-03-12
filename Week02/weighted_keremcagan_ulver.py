46b548dff7e07444f7c9daf51ca0769d58ab2f1e
Kerem Çağan Ülver <kerulver@hotmail.com>
2026-03-12 17:42:38 +0300

    Add weighted sampling function to weighted_keremcagan_ulver.py

---
+++ Week02/weighted_keremcagan_ulver.py
@@ -0,0 +1,10 @@
+import random
+
+def weighted_srs(data, n, weights, with_replacement):
+    if with_replacement: return random.choices(data, weights=weights, k=n)
+    res, w = [], list(weights)
+    for _ in range(n):
+        idx = random.choices(range(len(data)), weights=w)[0]
+        res.append(data[idx])
+        w[idx] = 0
+    return res
