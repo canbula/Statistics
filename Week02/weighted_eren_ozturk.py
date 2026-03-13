ebb960dc0d7c74dd64a643684c6908deff408494
EREN ÖZTÜRK <230316034@ogr.cbu.edu.tr>
2026-03-13 20:35:48 +0300

    Add default value for with_replacement parameter

--- Week02/weighted_eren_ozturk.py
+++ Week02/weighted_eren_ozturk.py
@@ -1,5 +1,5 @@
 import random
-def weighted_srs(data,n,weights,with_replacement):
+def weighted_srs(data,n,weights,with_replacement = False):
     if with_replacement: return random.choices(data,weights=weights,k=n)
     sample,remaining_data,remaining_weights =[],list(data),list(weights)
     for _ in range(n):
