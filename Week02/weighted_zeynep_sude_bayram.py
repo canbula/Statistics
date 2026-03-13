92a6751fadccbd2486828fec4ac0bf9b9ec1260f
Zeynep Sude BAYRAM <144821836+Zeyneepsudee@users.noreply.github.com>
2026-03-13 10:58:59 +0300

    Update weighted_zeynep_sude_bayram.py

--- Week02/weighted_zeynep_sude_bayram.py
+++ Week02/weighted_zeynep_sude_bayram.py
@@ -1,4 +1,6 @@
+import random
+
 def weighted_srs(data, n, weights, with_replacement=False):
-    if with_replacement: return random.choices(data, weights=weights, k=n) 
-if not weights: return random.sample(data, n) 
-return random.sample(data, n, counts=weights) 
+    if with_replacement: return random.choices(data, weights=weights, k=n) # sample with replacement
+    if not weights: return random.sample(data, n) # simple random sample without weights
+    return random.sample(data, n, counts=weights) # sample without replacement using weights as counts
