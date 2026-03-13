c01f6c529828a65561a07bf2fc3a26874526406c
elifgulseren <elifgulseren88@gmail.com>
2026-03-13 10:57:26 +0300

    Update weighted_elif_gulseren.py

--- Week02/weighted_elif_gulseren.py
+++ Week02/weighted_elif_gulseren.py
@@ -1,7 +1,6 @@
 import random
 def weighted_srs(data, n, weights, with_replacement=False):
-    if with_replacement:
-        return random.choices(data, weights=weights, k=n)
+    if with_replacement: return random.choices(data, weights=weights, k=n)
     res, w = [], list(weights)
     for _ in range(n):
         i = random.choices(range(len(data)), weights=w)[0]
