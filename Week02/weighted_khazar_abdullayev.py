bac89ab264c09560f019f09f41e4d7c2abde9b00
Khazar Abdullayev <emperorkhazar1299@gmail.com>
2026-03-13 10:40:17 +0300

    Update weighted_khazar_abdullayev.py

--- Week02/weighted_khazar_abdullayev.py
+++ Week02/weighted_khazar_abdullayev.py
@@ -1,7 +1,7 @@
 import random
-def weighted_srs(data, n, weights, with_replacement=False):
-    if with_replacement: return random.choices(data, weights=weights, k=n)
-    s, w = [], list(weights)
+def weighted_srs(data, n, weights=None, *, with_replacement=False):
+    w, s = list(weights) if weights else [1]*len(data), []
+    if with_replacement: return random.choices(data, weights=w, k=n)
     for _ in range(n):
         idx = random.choices(range(len(data)), weights=w)[0]
         s.append(data[idx])
