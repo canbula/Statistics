8bdb7f003f5e699150432e252333ddf55239c184
Ömer <230315035@ogr.cbu.edu.tr>
2026-03-13 10:34:56 +0300

    Update weighted_omer_kayaalp.py

--- Week02/weighted_omer_kayaalp.py
+++ Week02/weighted_omer_kayaalp.py
@@ -1,10 +1,9 @@
-def weighted_srs(data, n, weights, with_replacement):
+def weighted_srs(population, weights, k, *, with_replacement=False):
     import random
     if with_replacement:
-        return random.choices(data, weights=weights, k=n)
-    res=[]
-    d=data[:]; w=weights[:]
-    for _ in range(n):
-        i=random.choices(range(len(d)), weights=w, k=1)[0]
-        res.append(d.pop(i)); w.pop(i)
-    return res
+        return random.choices(population, weights=weights, k=k)
+    r=[]; p=population[:]; w=weights[:]
+    for _ in range(k):
+        i=random.choices(range(len(p)), weights=w)[0]
+        r.append(p.pop(i)); w.pop(i)
+    return r
