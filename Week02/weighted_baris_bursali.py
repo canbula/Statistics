159e404a762724f4476b35dd7e57545a06b8e789
Barış <barisbursali1987@gmail.com>
2026-03-13 09:51:12 +0300

    Fix function signature and line count

--- Week02/weighted_baris_bursali.py
+++ Week02/weighted_baris_bursali.py
@@ -1,10 +1,10 @@
 import random
 
-def weighted_srs(data, n, weights, with_replacement):
-    w, out = list(weights) if weights else [1] * len(data), []
+def weighted_srs(data, n, weights, *, with_replacement=False):
+    w, out = (weights[:] if weights else [1]*len(data)), []
     for _ in range(n):
-        target, current = random.uniform(0, sum(w)), 0
-        for i, val in enumerate(w):
-            current += val
-            if target <= current: out.append(data[i]); w[i] *= int(with_replacement); break
+        r = random.uniform(0, sum(w))
+        for i in range(len(data)):
+            r -= w[i]
+            if r <= 0: out.append(data[i]); w[i] *= int(with_replacement); break
     return out
