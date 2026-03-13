9ac355200da0407cc5a20c02a86b3615f6537483
Yunus Dermencioğlu <yunusdermencioglu@gmail.com>
2026-03-13 10:09:57 +0300

    Update weighted_yunus_dermencioglu.py

--- Week02/weighted_yunus_dermencioglu.py
+++ Week02/weighted_yunus_dermencioglu.py
@@ -1,9 +1,10 @@
 import random
 
-def weighted_srs(data, n, weights, with_replacement=True):
-    if with_replacement: return random.choices(data, weights=weights, k=n)  # allows repeats
+def weighted_srs(data, n, weights, *args, with_replacement=False):
+    if args: with_replacement = args[0]  # also accept 4th positional arg if given
+    if with_replacement: return random.choices(data, weights=weights, k=n)  # repeats allowed
     w, res = list(weights), []
     for _ in range(n):
-        i = random.choices(range(len(data)), weights=w)[0]  # pick by current weights
+        i = random.choices(range(len(data)), weights=w)[0]
         res.append(data[i]); w[i] = 0
     return res
