c8a1e76ffab04f84d63a5a8c862f61c5ffdfb161
Khazar Abdullayev <emperorkhazar1299@gmail.com>
2026-03-13 10:46:10 +0300

    Refactor weighted_srs function implementation
    Refactor weighted_srs function to handle weights and replacement more clearly.

--- Week02/weighted_khazar_abdullayev.py
+++ Week02/weighted_khazar_abdullayev.py
@@ -1,9 +1,9 @@
 import random
-def weighted_srs(data, n, weights=None, *, with_replacement=False):
-    w, s = list(weights) if weights else [1]*len(data), []
-    if with_replacement: return random.choices(data, weights=w, k=n)
-    for _ in range(n):
-        idx = random.choices(range(len(data)), weights=w)[0]
-        s.append(data[idx])
-        w[idx] = 0
-    return s
+
+def weighted_srs(data, n, weights, *args, with_replacement=False):
+    if args: with_replacement = args[0]
+    if with_replacement: 
+        return random.sample(data, k=n)
+    
+    w = weights if weights else [1] * len(data)
+    return random.choices(data, weights=w, k=n)
