2f10ee6d7caf2baf17160993753c4ef51e67b055
EREN ÖZTÜRK <230316034@ogr.cbu.edu.tr>
2026-03-13 21:21:06 +0300

    Refactor weighted_srs function for improved logic

--- Week02/weighted_eren_ozturk.py
+++ Week02/weighted_eren_ozturk.py
@@ -1,9 +1,9 @@
 import random
-def weighted_srs(data,n,weights,with_replacement = False):
-    if with_replacement: return random.choices(data,weights=weights,k=n)
-    sample,remaining_data,remaining_weights =[],list(data),list(weights)
+def weighted_srs(data, n, weights, with_replacement=False):
+    if weights or with_replacement: return random.choices(data, weights=weights, k=n)
+    sample, remaining_data = [], list(data)
     for _ in range(n):
-        chosen_item = random.choices(remaining_data,weights=remaining_weights)[0]
-        sample.append(chosen_item)
-        remaining_weights.pop(remaining_data.index(chosen_item)); remaining_data.remove(chosen_item)
+        chosen = random.choices(remaining_data)[0]
+        sample.append(chosen)
+        remaining_data.remove(chosen)
     return sample
