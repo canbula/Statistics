25dac0c0ceb1e84ee3cc495cd394278370940838
denizakgl <dnzakg18@gmail.com>
2026-03-13 14:25:47 +0300

    Update weighted_deniz_akgul.py

--- Week02/weighted_deniz_akgul.py
+++ Week02/weighted_deniz_akgul.py
@@ -1,9 +1,5 @@
 import random
 def weighted_srs(data, n, weights, with_replacement=False):
-    if with_replacement: return random.choices(data, weights=weights, k=n)
-    d, w, res = list(data), list(weights) if weights else None, []
-    for _ in range(n):
-        i = random.choices(range(len(d)), weights=w)[0]
-        res.append(d.pop(i))
-        if w is not None: w.pop(i)
-    return res
+    if with_replacement or weights:
+        return random.choices(data, weights=weights, k=n)
+    return random.sample(data, n)
