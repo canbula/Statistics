3af2aad2d29317329b744681abfcb2c1e72a74d3
Rıza Arslan <70386445+ragg211@users.noreply.github.com>
2026-03-13 13:13:43 +0300

    Fix formatting and remove unnecessary line breaks

--- Week02/weighted_riza_arslan.py
+++ Week02/weighted_riza_arslan.py
@@ -1,5 +1,4 @@
-student_id = "244101020"
-full_name = "Rıza Arslan"
+student_id = "244101020" full_name = "Rıza Arslan"
 import random
 def weighted_srs(data, n, weights, with_replacement=False):
     w = weights if weights else [1]*len(data)
