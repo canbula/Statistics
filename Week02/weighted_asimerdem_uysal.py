c9933ac0e7cdefd94ea148a797e179025cd4b426
asimerdwm <seherazraerdem@gmail.com>
2026-03-13 10:15:01 +0300

    Refactor weighted_srs function for better sampling

--- Week02/weighted_asimerdem_uysal.py
+++ Week02/weighted_asimerdem_uysal.py
@@ -1,7 +1,10 @@
-import random 
-
-def weighted_srs(data,n,weights,with_replacement):
-  if with_replacement == True: 
-    return random.choices(data,weights=weights,k=n)
-  else :
-    return random.sample(data,k=n)
+import random
+def weighted_srs(data, n, weights, with_replacement):
+    if with_replacement:
+        return random.choices(data, weights=weights, k=n)
+    d, w, res = list(data), list(weights), []
+    for _ in range(n):
+        i = random.choices(range(len(d)), weights=w)[0]
+        res.append(d.pop(i))
+        w.pop(i)
+    return res
