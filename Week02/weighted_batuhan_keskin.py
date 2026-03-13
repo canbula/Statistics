9efd0b3c22191fe064f2f41a6ea2fb27a41d759a
Batuhan Keskin <121479880+batyou0ne@users.noreply.github.com>
2026-03-13 10:30:27 +0300

    Refactor weighted__batuhan_keskin.py

--- Week02/weighted_batuhan_keskin.py
+++ Week02/weighted_batuhan_keskin.py
@@ -1,6 +1,6 @@
 import random
 
-def weighted_srs(data, n, weights=None, with_replacement=False):
-    if with_replacement: return random.choices(data, weights=weights, k=n) #sample with replacement
-    if not weights: return random.sample(data, n) #simple random sample without weights
-    return random.sample(data, n, counts=weights) #sample without replacement using weights as counts
+def weighted_srs(data, n, weights, with_replacement=False):
+    if with_replacement: return random.choices(data, weights=weights, k=n) # sample with replacement
+    if not weights: return random.sample(data, n) # simple random sample without weights
+    return random.sample(data, n, counts=weights) # sample without replacement using weights as counts
