9f1a75b78e5b394df1cc4faa0c39e7428b8a9d7b
asimerdwm <seherazraerdem@gmail.com>
2026-03-13 10:42:48 +0300

    Update weighted_asimerdem_uysal.py

--- Week02/weighted_asimerdem_uysal.py
+++ Week02/weighted_asimerdem_uysal.py
@@ -1,8 +1,6 @@
 import random
+
 def weighted_srs(data, n, weights, with_replacement=False):
-    if with_replacement: return random.choices(data, weights=weights, k=n)
-    d, w, res = list(data), list(weights), []
-    for _ in range(n):
-        i = random.choices(range(len(d)), weights=w)[0]
-        res.append(d.pop(i)); w.pop(i)
-    return res
+    if with_replacement: return random.choices(data, weights=weights, k=n) # sample with replacement
+    if not weights: return random.sample(data, n) # simple random sample without weights
+    return random.sample(data, n, counts=weights) # sample without replacement using weights as counts
