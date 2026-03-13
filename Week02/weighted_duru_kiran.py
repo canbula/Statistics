9ba0ca9e3fb7a88035f4050cfeadedb44b78b5fb
duru୨ৎ <ddurukiran@gmail.com>
2026-03-13 10:02:02 +0300

    Create weighted_duru_kiran.py

---
+++ Week02/weighted_duru_kiran.py
@@ -0,0 +1,9 @@
+import random
+def weighted_srs(data, n, weights, with_replacement=False):
+    if with_replacement: return random.choices(data, weights=weights, k=n)
+    res, d, w = [], list(data), list(weights)
+    for _ in range(n):
+        index = random.choices(range(len(d)), weights=w, k=1)[0]
+        res.append(d.pop(index))
+        w.pop(index)
+    return res
