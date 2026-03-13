eec4bb14668a421c851fc316739fcbcf9e5d37a7
Bora Canbula <bora.canbula@cbu.edu.tr>
2026-03-13 09:31:29 +0300

    Add empty line before weighted_srs function

--- Week02/weighted_caner_gundas.py
+++ Week02/weighted_caner_gundas.py
@@ -1,5 +1,6 @@
 import random
 
+
 def weighted_srs(data, n, weights, with_replacement):
     if with_replacement: return random.choices(data, weights=weights, k=n)
     res, d, w = [], list(data), list(weights)
