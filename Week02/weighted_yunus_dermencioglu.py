2566a1563af3851295dc223c57bbb36975329f2d
Yunus Dermencioğlu <yunusdermencioglu@gmail.com>
2026-03-13 10:05:25 +0300

    Update weighted_yunus_dermencioglu.py

--- Week02/weighted_yunus_dermencioglu.py
+++ Week02/weighted_yunus_dermencioglu.py
@@ -1,9 +1,9 @@
 import random
-def weighted_srs(data, n, weights, with_replacement):
-    if with_replacement: return random.choices(data, weights=weights, k=n) # If sampling with replacement, we can directly use random.choices
+def weighted_srs(data, n, weights, *, with_replacement=False):
+    if with_replacement: return random.choices(data, weights=weights, k=n)  # sampling with replacement
     w, res = list(weights), []
     for _ in range(n):
-        idx = random.choices(range(len(data)), weights=w)[0] # choose an index based on the current weight distribution
+        idx = random.choices(range(len(data)), weights=w)[0]  # pick index based on weights
         res.append(data[idx])
         w[idx] = 0
     return res
