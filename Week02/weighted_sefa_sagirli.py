28d90cb1fa98d3879160c367b63d781118ceef5b
Sefa Sağırlı <143504944+sefas24@users.noreply.github.com>
2026-03-13 10:56:04 +0300

    Fix typo in weighted_srs function parameter name

--- Week02/weighted_sefa_sagirli.py
+++ Week02/weighted_sefa_sagirli.py
@@ -1,10 +1,10 @@
 import random 
-def weighted_srs(data, n, weigths, with_replacement):
+def weighted_srs(data, n, weights, with_replacement):
   if with_replacement == True: return random.choices(data, weights=weights, k=n)
-    = list(weights)
+    newList = list(weights)
 result = []
 for i in range(n):
-  chosen_index = random.choices(range(len(data)), weigths = copy, k=1)[0] 
+  chosen_index = random.choices(range(len(data)), weigths = newList, k=1)[0] 
 result.append(data[chosen_index])
-copy[chosen_index] = 0
+newList[chosen_index] = 0
 return result
