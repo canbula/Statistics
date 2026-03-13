2ca4e3b4ee310ca0fbe59e221ea3a4cc6b2e8a1b
Sefa Sağırlı <143504944+sefas24@users.noreply.github.com>
2026-03-13 11:00:08 +0300

    Fix indentation and typo in weighted_srs function

--- Week02/weighted_sefa_sagirli.py
+++ Week02/weighted_sefa_sagirli.py
@@ -1,10 +1,10 @@
-import random 
+import random
 def weighted_srs(data, n, weights, with_replacement):
-  if with_replacement == True: return random.choices(data, weights=weights, k=n)
-  newList = list(weights)
-result = []
-for i in range(n):
-  chosen_index = random.choices(range(len(data)), weigths = newList, k=1)[0] 
-result.append(data[chosen_index])
-newList[chosen_index] = 0
-return result
+    if with_replacement == True: return random.choices(data, weights=weights, k=n)
+    newList = list(weights)
+    result = []
+    for i in range(n):
+        chosen_index = random.choices(range(len(data)), weights=newList, k=1)[0]
+        result.append(data[chosen_index])
+        newList[chosen_index] = 0
+    return result
