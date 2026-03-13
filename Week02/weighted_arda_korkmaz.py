fd7a913dc7f35461d75d8a9e1848c00077b5d541
Arda Korkmaz <143834039+Ardakorkmaz0@users.noreply.github.com>
2026-03-13 18:05:14 +0300

    Change weighted_srs to not allow replacement by default

--- Week02/weighted_arda_korkmaz.py
+++ Week02/weighted_arda_korkmaz.py
@@ -1,10 +1,9 @@
 import random
-def weighted_srs(data, n, weights, with_replacement=True):
+def weighted_srs(data, n, weights, with_replacement=False):
   result = []
   for i in range(n):
     idx = data.index(random.choices(data, weights=weights, k=1)[0])
     result.append(data[idx])
     if not with_replacement:
-      data.pop(idx)
-      weights.pop(idx)
+      data.pop(idx); weights.pop(idx) if weights else None
   return result
