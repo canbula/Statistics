70256d873825074c241fda7c77159714dcb77dac
duru୨ৎ <ddurukiran@gmail.com>
2026-03-13 09:48:01 +0300

    Update weighted_duru_kiran.py

--- Week02/weighted_duru_kiran.py
+++ Week02/weighted_duru_kiran.py
@@ -1,6 +1,7 @@
 import random
-def weighted_srs(data, n, weights, *, with_replacement=False):
-    if with_replacement: return random.choices(data, weights=weights, k=n)
+def weighted_srs(data, n, weights, with_replacement=False):
+    if with_replacement:
+        return random.choices(data, weights=weights, k=n)
     selected, w = [], list(weights)
     for _ in range(n):
         index = random.choices(range(len(data)), weights=w)[0]
