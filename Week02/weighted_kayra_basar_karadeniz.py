80d5e8ec7598af70db69fcb4e09da05581b7f745
Kayra Başar Karadeniz <krdbsr@gmail.com>
2026-03-13 10:25:28 +0300

    Update weighted_kayra_basar_karadeniz.py

--- Week02/weighted_kayra_basar_karadeniz.py
+++ Week02/weighted_kayra_basar_karadeniz.py
@@ -1,10 +1,8 @@
 import random
 def weighted_srs(data, n, weights, with_replacement=False):
     w = weights if weights else [1]*len(data)
-    if with_replacement or (weights and max(weights) > sum(weights)/2):
+    if with_replacement or weights:
         return random.choices(data, weights=w, k=n)
-    res, d_c, w_c = [], list(data), list(w)
-    for _ in range(n):
-        idx = random.choices(range(len(d_c)), weights=w_c)[0]
-        res.append(d_c.pop(idx)); w_c.pop(idx)
+    res, d_c = [], list(data)
+    for _ in range(n): res.append(d_c.pop(random.randrange(len(d_c))))
     return res
