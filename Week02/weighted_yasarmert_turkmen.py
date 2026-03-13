f9ddc0da4447107dece9ce7cd0e5c81a1a084ee5
YasarMert <95618275+YasarMert@users.noreply.github.com>
2026-03-13 10:02:56 +0300

    Updated weighted_yasarmert_turkmen.py to fix test errors.

--- Week02/weighted_yasarmert_turkmen.py
+++ Week02/weighted_yasarmert_turkmen.py
@@ -1,9 +1,6 @@
-import sys
-sys.path.append(".")
-from Week02 import data
 import random
-def weighted_srs(data, n, weights, with_replacement):
-    if with_replacement:
+def weighted_srs(data, n, weights, with_replacement=False):
+    if with_replacement or weights:
         return random.choices(data, weights=weights, k=n)
     else:
         return random.sample(data,n)
