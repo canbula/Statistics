6e4b0b0cabeb23606b25f837af2cff909ce64fb6
Mert İşgüzar <156206580+AcilardVII@users.noreply.github.com>
2026-03-13 21:12:54 +0300

    Delete Week02/weighted_yasarmert_turkmen.py

--- Week02/weighted_yasarmert_turkmen.py
+++
@@ -1,6 +0,0 @@
-import random
-def weighted_srs(data, n, weights, with_replacement=False):
-    if with_replacement or weights:
-        return random.choices(data, weights=weights, k=n)
-    else:
-        return random.sample(data,n)
