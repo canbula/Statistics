e2af3c4819f3a9f6c2893e1e39f6b78be76b9133
Zeynep Sude BAYRAM <144821836+Zeyneepsudee@users.noreply.github.com>
2026-03-13 10:44:25 +0300

    Update weighted_zeynep_sude_bayram.py

--- Week02/weighted_zeynep_sude_bayram.py
+++ Week02/weighted_zeynep_sude_bayram.py
@@ -1,7 +1,6 @@
 import random
 def weighted_srs(data, n, weights, with_replacement=False):
-    if with_replacement:
-        return random.choices(data, weights=weights, k=n)
+    if with_replacement: return random.choices(data, weights=weights, k=n)
     res, d, w = [], list(data), list(weights)
     for _ in range(n):
         idx = random.choices(range(len(d)), weights=w, k=1)[0]
