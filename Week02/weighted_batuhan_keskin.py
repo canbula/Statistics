943581e90fad33a8dec6a6f16e7f1b4b0af3dd28
Batuhan Keskin <121479880+batyou0ne@users.noreply.github.com>
2026-03-13 10:06:18 +0300

    Updated the rows number weighted_batuhan_keskin.py

--- Week02/weighted_batuhan_keskin.py
+++ Week02/weighted_batuhan_keskin.py
@@ -1,18 +1,10 @@
 import random
-
 def weighted_srs(data, n, weights, with_replacement):
-    # If sampling with replacement directly use random.choices
     if with_replacement:
         return random.choices(data, weights=weights, k=n)
-
-    #copy  lists so original data is not modified
-    sample, d, w = [], data[:], weights[:]
-
-    #select n elements based on their weights
-    for _ in range(n):
-        x = random.choices(d, weights=w, k=1)[0]  #pick one element
-        i = d.index(x)                            # find its index
-        sample.append(x)                          #add to sample
-        d.pop(i); w.pop(i)                        #remove to avoid reselection
-
+    sample=[]     #for picking without replacement
+    d=data[:]; w=weights[:]
+    for _ in range(n):   #we need to pick n items in total
+        x=random.choices(d, weights=w, k=1)[0]   #grab just 1 item based on the current weights
+        i=d.index(x); sample.append(x); d.pop(i); w.pop(i)   #find it, add to our sample, and pop it out so it doesn't get picked again
     return sample
