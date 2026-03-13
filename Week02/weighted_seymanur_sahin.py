b19392cf760aa4aca97cc659b2fdd8246cb603c7
shnseymanr <sahinseymanur123@gmail.com>
2026-03-13 11:03:07 +0300

    Update weighted_seymanur_sahin.py

--- Week02/weighted_seymanur_sahin.py
+++ Week02/weighted_seymanur_sahin.py
@@ -1,9 +1,8 @@
 import random
 def weighted_srs(data, n, weights, with_replacement=False):
-    if with_replacement: return random.choices(data, weights=weights, k=n)
-    result, temp_data, temp_weights = [], list(data), list(weights or [1]*len(data))
+    if with_replacement or weights: return random.choices(data, weights=weights, k=n)
+    result, temp_data = [], list(data)
     for _ in range(n):
-        target_index = random.choices(range(len(temp_data)), weights=temp_weights, k=1)[0]
+        target_index = random.randrange(len(temp_data))
         result.append(temp_data.pop(target_index))
-        temp_weights.pop(target_index)
     return result
