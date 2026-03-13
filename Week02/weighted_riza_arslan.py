345ab9f27cf9b0e5142970eff35bd231e546df5d
Rıza Arslan <70386445+ragg211@users.noreply.github.com>
2026-03-13 10:29:00 +0300

    Refactor weighted_srs function for clarity

--- Week02/weighted_riza_arslan.py
+++ Week02/weighted_riza_arslan.py
@@ -1,8 +1,9 @@
 student_id = "244101020"
 full_name = "Rıza Arslan"
 import random
-def weighted_srs(data, n, weights, *,with_replacement=False):
+def weighted_srs(data, n, weights, with_replacement=False):
     if with_replacement:
         return random.choices(data, weights=weights, k=n)
     else:
         return random.sample(data, n, weights=weights)
+    
