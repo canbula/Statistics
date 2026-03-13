d8884722af76b1de6d866d54e6276117cf1fd495
Mert İşgüzar <156206580+AcilardVII@users.noreply.github.com>
2026-03-13 21:14:19 +0300

    Update weighted_mert_isguzar.py

--- Week02/weighted_mert_isguzar.py
+++ Week02/weighted_mert_isguzar.py
@@ -1,5 +1,4 @@
 import random
-
 def weighted_srs(data, n, weights, with_replacement = False):
     if with_replacement == True or weights != None:
         return random.choices(data, k = n, weights = weights)
