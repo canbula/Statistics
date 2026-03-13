a758a3d5d4bb1a082d4428c98149926250e8197f
Kayra Başar Karadeniz <krdbsr@gmail.com>
2026-03-13 10:14:36 +0300

    Add support for sampling with replacement

--- Week02/weighted_kayra_basar_karadeniz.py
+++ Week02/weighted_kayra_basar_karadeniz.py
@@ -1,8 +1,10 @@
 import random
 def weighted_srs(data, n, weights, with_replacement=False):
+    if with_replacement:
+        return random.choices(data, weights=weights, k=n)
     res, d_copy, w_copy = [], list(data), list(weights) if weights else [1]*len(data)
     for _ in range(n):
         idx = random.choices(range(len(d_copy)), weights=w_copy)[0]
-        res.append(d_copy[idx] if with_replacement else d_copy.pop(idx))
-        if not with_replacement: w_copy.pop(idx)
+        res.append(d_copy.pop(idx))
+        w_copy.pop(idx)
     return res
