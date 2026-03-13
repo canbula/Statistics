ef2f6816d1da2b99272e90f137edc446c4e061ad
Mert İşgüzar <156206580+AcilardVII@users.noreply.github.com>
2026-03-13 21:21:53 +0300

    Refactor weighted_srs to use res instead of r

--- Week02/weighted_mert_isguzar.py
+++ Week02/weighted_mert_isguzar.py
@@ -1,8 +1,10 @@
 import random
-def weighted_srs(data, n, weights, with_replacement=False):
+def weighted_srs(data, n, weights=None, with_replacement=False):
     if with_replacement: return random.choices(data, weights=weights, k=n)
     if not weights: return random.sample(data, n)
-    d, w, r = list(data), list(weights), []
+    d, w, res = list(data), list(weights), []
     for _ in range(n):
-        i = random.choices(range(len(d)), weights=w)[0]; r.append(d.pop(i)); w.pop(i)
-    return r
+        i = random.choices(range(len(d)), weights=w)[0]
+        res.append(d.pop(i))
+        w.pop(i)
+    return res
