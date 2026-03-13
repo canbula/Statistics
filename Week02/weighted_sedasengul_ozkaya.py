d7e84236606689731d460469e4497ae1f2128d4b
sedaozkaya <155262567+sedaozkaya@users.noreply.github.com>
2026-03-13 09:59:18 +0300

    Add weighted sampling function with optional replacement

---
+++ Week02/weighted_sedasengul_ozkaya.py
@@ -0,0 +1,12 @@
+import random
+
+def weighted_srs(data, n, weights, with_replacement=False):
+    if with_replacement:
+        return random.choices(data, weights=weights, k=n)
+    
+    res, d, w = [], list(data), list(weights)
+    for _ in range(n):
+        idx = random.choices(range(len(d)), weights=w, k=1)[0]
+        res.append(d.pop(idx))
+        w.pop(idx)
+    return res
