3e6ebc2b9506de222d2615951560be3e41aff13c
MelisaNurHamamci <melisahamamci87@gmail.com>
2026-03-13 10:00:39 +0300

    Create weighted_melisanur_hamamci.py

---
+++ Week02/weighted_melisanur_hamamci.py
@@ -0,0 +1,10 @@
+import random
+def weighted_srs(data, n, weights, with_replacement):
+    if with_replacement:
+        return random.choices(data, weights=weights, k=n)
+    res, d, w = [], list(data), list(weights)
+    for _ in range(n):
+        pick = random.choices(d, weights=w, k=1)[0]
+        idx = d.index(pick)
+        res.append(d.pop(idx)); w.pop(idx)
+    return res
