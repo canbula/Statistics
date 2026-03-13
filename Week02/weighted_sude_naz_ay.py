ff9e4f91b0bb413fc488e473dd7938146d9d2d46
sudenazy7 <sudenazx00@gmail.com>
2026-03-13 10:47:53 +0300

    Update weighted_sude_naz_ay.py

--- Week02/weighted_sude_naz_ay.py
+++ Week02/weighted_sude_naz_ay.py
@@ -1,9 +1,9 @@
 import random
+
 def weighted_srs(data, n, weights, with_replacement=False):
-    data, w, res = list(data), (list(weights) if weights else None), []
-    if with_replacement: return random.choices(data, weights=w, k=n)
-    for _ in range(n):
-        c = random.choices(data, weights=w, k=1)[0]; res.append(c)
-        idx = data.index(c); data.pop(idx)
-        if w: w.pop(idx)
-    return res
+    if weights is None:
+        return random.choices(data, k=n) if with_replacement else random.sample(data, n)
+    if with_replacement:
+        return random.choices(data, weights=weights, k=n)
+    population = sum([[d]*w for d, w in zip(data, weights)], [])
+    return random.sample(population, n)
