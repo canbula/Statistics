9cd947b4ecaf897c7bcb584089293098a7b1bf23
Bora Canbula <bora.canbula@cbu.edu.tr>
2026-03-13 09:28:33 +0300

    Import random module in weighted_deniz_akgul.py
    Added import statement for random module.

--- Week02/weighted_deniz_akgul.py
+++ Week02/weighted_deniz_akgul.py
@@ -1,4 +1,5 @@
 import random
+
 def weighted_srs(data, n, weights, with_replacement=False):
     if with_replacement: return random.choices(data, weights=weights, k=n)
     d, w, res = list(data), list(weights) if weights else None, []
