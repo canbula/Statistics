0bac4b817431271613558d8d29710829d0e0a289
Batuhan Keskin <121479880+batyou0ne@users.noreply.github.com>
2026-03-13 10:24:14 +0300

    Refactor weighted_batuhan_keskin.py

--- Week02/weighted_batuhan_keskin.py
+++ Week02/weighted_batuhan_keskin.py
@@ -1,9 +1,6 @@
 import random
-def weighted_srs(data, n, weights, *, with_replacement=False):
-    if with_replacement: return random.choices(data, weights=weights, k=n) #easy path if replacement is on
-    d, w, sample = data[:], weights[:], [] #we make copies so we don't mess up the original data
-    for _ in range(n):
-        i = d.index(random.choices(d, weights=w)[0]) #pick one based on weights and find its index
-        sample.append(d.pop(i)) #put it in the sample list remove from data
-        w.pop(i) #remove the matching weight so we don't break the balance
-    return sample
+
+def weighted_srs(data, n, weights=None, with_replacement=False):
+    if with_replacement: return random.choices(data, weights=weights, k=n) #sample with replacement
+    if not weights: return random.sample(data, n) #simple random sample without weights
+    return random.sample(data, n, counts=weights) #sample without replacement using weights as counts
