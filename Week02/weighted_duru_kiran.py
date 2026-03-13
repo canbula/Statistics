d9a228ebeb112e27674f31d35a30bc13e8213d42
duru୨ৎ <ddurukiran@gmail.com>
2026-03-13 09:52:39 +0300

    Update weighted_duru_kiran.py

--- Week02/weighted_duru_kiran.py
+++ Week02/weighted_duru_kiran.py
@@ -2,9 +2,9 @@ import random
 def weighted_srs(data, n, weights, with_replacement=False):
     if with_replacement:
         return random.choices(data, weights=weights, k=n)
-    selected, w = [], list(weights)
+    res, d, w = [], list(data), list(weights)
     for _ in range(n):
-        index = random.choices(range(len(data)), weights=w)[0]
-        selected.append(data[index])
-        w[index] = 0
-    return selected
+        index = random.choices(range(len(d)), weights=w, k=1)[0]
+        res.append(d.pop(index))
+        w.pop(index)
+    return res
