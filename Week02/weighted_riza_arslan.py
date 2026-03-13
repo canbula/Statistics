5b0953648c01ee7dbc47c1dedb25ddc95bfd62ec
Rıza Arslan <70386445+ragg211@users.noreply.github.com>
2026-03-13 10:25:52 +0300

    Add keyword-only argument for with_replacement

--- Week02/weighted_riza_arslan.py
+++ Week02/weighted_riza_arslan.py
@@ -1,9 +1,8 @@
 student_id = "244101020"
 full_name = "Rıza Arslan"
 import random
-def weighted_srs(data, n, weights, with_replacement=True):
+def weighted_srs(data, n, weights, *,with_replacement=False):
     if with_replacement:
         return random.choices(data, weights=weights, k=n)
     else:
         return random.sample(data, n, weights=weights)
-    
