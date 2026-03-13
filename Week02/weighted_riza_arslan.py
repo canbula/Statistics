e21b9e75f3d7416a23ab05afbbb7634a2a773b73
Rıza Arslan <70386445+ragg211@users.noreply.github.com>
2026-03-13 12:27:23 +0300

    Fix weighted_srs function to use random.choice correctly

--- Week02/weighted_riza_arslan.py
+++ Week02/weighted_riza_arslan.py
@@ -5,6 +5,6 @@ def weighted_srs(data, n, weights, with_replacement=False):
     if with_replacement:
         return random.choices(data, weights=weights, k=n)
     else:
-        return random.choice(data, weights=None, k=n)
+        return random.choice(data,  k=n)
         
     
