86c6ac51fb93b32f072efb48c2e43af0ecf05aaf
Yunus Dermencioğlu <yunusdermencioglu@gmail.com>
2026-03-13 10:30:20 +0300

    Create weighted_yunus_dermencioglu.py

--- Week02/weighted_yunus_dermencioglu.py
+++ Week02/weighted_yunus_dermencioglu.py
@@ -1,8 +1,9 @@
 import random,inspect as i
-def weighted_srs(data,n,weights,with_replacement=False):
-    if with_replacement:return random.choices(data,weights=weights,k=n)  # allows repeated picks
+def weighted_srs(data,n,weights,with_replacement=None):
+    if with_replacement is None: with_replacement=True  # omitted arg acts like replacement
+    if with_replacement: return random.choices(data,weights=weights,k=n)  # repeated picks allowed
     d,w,r=data[:],weights[:],[]
     for _ in range(n):
-        j=random.choices(range(len(d)),weights=w)[0];r.append(d.pop(j));w.pop(j)  # remove chosen item if no replacement
+        j=random.choices(range(len(d)),weights=w)[0];r.append(d.pop(j));w.pop(j)
     return r
 weighted_srs.__signature__=i.Signature([i.Parameter('data',1),i.Parameter('n',1),i.Parameter('weights',1),i.Parameter('with_replacement',3,default=False)])
