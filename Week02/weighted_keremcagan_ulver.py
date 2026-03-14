700c7b1c418b4e2295daabdc0a5f0d520273ac3a
Kerem Çağan Ülver <kerulver@hotmail.com>
2026-03-14 10:32:04 +0300

    Refactor weighted_srs function to use keyword arguments

--- Week02/weighted_keremcagan_ulver.py
+++ Week02/weighted_keremcagan_ulver.py
@@ -1,11 +1,8 @@
 import random
-
-
-def weighted_srs(data, n, weights, with_replacement):
+def weighted_srs(data, n, weights, *, with_replacement=False):
     if with_replacement: return random.choices(data, weights=weights, k=n)
     res, w = [], list(weights)
     for _ in range(n):
         idx = random.choices(range(len(data)), weights=w)[0]
-        res.append(data[idx])
-        w[idx] = 0
+        res.append(data[idx]); w[idx] = 0
     return res
