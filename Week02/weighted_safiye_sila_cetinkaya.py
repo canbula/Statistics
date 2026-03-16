6b44e9d534d23dfd4aaa04ef3e8dc5bdfe6a682f
silacetinkaya7 <slacetinkaya98@gmail.com>
2026-03-16 23:05:40 +0300

    weighted_safiye_sila_cetinkaya.py

--- Week02/weighted_safiye_sila_cetinkaya.py
+++ Week02/weighted_safiye_sila_cetinkaya.py
@@ -1,7 +1,6 @@
 import random
 def weighted_srs(data, n, weights, with_replacement):
-    if with_replacement:
-        return random.choices(data, weights=weights, k=n)
+    if with_replacement: return random.choices(data, weights=weights, k=n)
     sample, data, weights = [], list(data), list(weights)
     for _ in range(n):
         r = random.uniform(0, sum(weights))
@@ -9,4 +8,3 @@ def weighted_srs(data, n, weights, with_replacement):
             r -= w
             if r <= 0: sample.append(data.pop(i)); weights.pop(i); break
     return sample
-  
