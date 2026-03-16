078fe39ec29ab0036ff3a03da1d93acd0522eb07
Rıza Arslan <70386445+ragg211@users.noreply.github.com>
2026-03-16 13:37:29 +0300

    Update weighted_riza_arslan.py

--- Week02/weighted_riza_arslan.py
+++ Week02/weighted_riza_arslan.py
@@ -1,10 +1,10 @@
 student_id = "244101020"
 full_name = "Rıza Arslan"
 import random
-def weighted_srs(data, n, weights=None, with_replacement=False):
+def weighted_srs(data, n, weights, with_replacement=False):
     w = weights if weights else [1]*len(data)
     if with_replacement or weights:
         return random.choices(data, weights=w, k=n)
     res, d_c = [], list(data)
     for _ in range(n): res.append(d_c.pop(random.randrange(len(d_c))))
-    return res
+    return res:
