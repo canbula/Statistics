a5c6ba9e15f06eb71fdc497e2f6187768a5264b0
Buğra Öztürk <ozturkbugra684@gmail.com>
2026-03-18 13:37:33 +0300

    Add weighted sampling function to weighted_bugra_ozturk.py

---
+++ Week02/weighted_bugra_ozturk.py
@@ -0,0 +1,8 @@
+import random
+def weighted_srs(data, n, weights, *, with_replacement=False):
+    if weights is None: weights = [1] * len(data)
+    population, _weights, sample = list(data), list(weights), []
+    while len(sample) < n:
+        picked = random.choices(population, weights=_weights, k=1)[0]
+        if with_replacement or picked not in sample: sample.append(picked)
+    return sample
