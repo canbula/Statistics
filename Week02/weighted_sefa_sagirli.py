57580c37ef7b4334ff18832b7cc127d0947d1b4b
Sefa Sağırlı <143504944+sefas24@users.noreply.github.com>
2026-03-18 13:50:02 +0300

    Refactor weighted_srs function for better sampling

--- Week02/weighted_sefa_sagirli.py
+++ Week02/weighted_sefa_sagirli.py
@@ -1,9 +1,7 @@
 import random
-def weighted_srs(data, n, weights, with_replacement=False):
-    if with_replacement: return random.choices(data, weights=weights, k=n)
-    newList, result = list(weights), []
-    for i in range(n):
-        idx = random.choices(range(len(data)), weights=newList, k=1)[0]
-        result.append(data[idx])
-        newList[idx] = 0
-    return result
+def weighted_srs(data, n, weights, with_replacement = False):
+    if not (with_replacement or weights):
+        sample = random.sample(data, n)
+    else:
+        sample = random.choices(data, weights=weights, k=n)
+    return sample
