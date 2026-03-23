e0817ae0644da2ddcc8303a191731c031e6a8da3
ENES DAĞ <mustafaenesdag@gmail.com>
2026-03-23 18:29:18 +0300

    Fix function signature: add default value for with_replacement

--- Week02/weighted_mustafaenes_dag.py
+++ Week02/weighted_mustafaenes_dag.py
@@ -1,8 +1,7 @@
 import random
 
-def weighted_srs(data, n, weights, *, with_replacement):
+def weighted_srs(data, n, weights, with_replacement=False):
     if with_replacement:
         return random.choices(data, weights=weights, k=n)
     else:
         return random.sample(data, k=n, counts=weights)
-# test 
