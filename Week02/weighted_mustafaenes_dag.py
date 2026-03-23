dd7f2db4dabe2d7e198ff4d4e94cd41c40a9d501
ENES DAĞ <mustafaenesdag@gmail.com>
2026-03-23 18:23:44 +0300

    Trigger workflow

--- Week02/weighted_mustafaenes_dag.py
+++ Week02/weighted_mustafaenes_dag.py
@@ -5,3 +5,4 @@ def weighted_srs(data, n, weights, *, with_replacement):
         return random.choices(data, weights=weights, k=n)
     else:
         return random.sample(data, k=n, counts=weights)
+# test 
