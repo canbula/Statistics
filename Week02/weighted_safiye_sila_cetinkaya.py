4f34ad56434defe7d6395cb9b2f326e8e8190980
silacetinkaya7 <slacetinkaya98@gmail.com>
2026-03-24 15:56:04 +0300

    weighted_safiye_sila_cetinkaya.py

--- Week02/weighted_safiye_sila_cetinkaya.py
+++ Week02/weighted_safiye_sila_cetinkaya.py
@@ -1,7 +1,6 @@
 import random
 
 def weighted_srs(data, n, weights, with_replacement=False):
-    if with_replacement or weights:
-        res = random.choices(data, weights=weights, k=n)
-        return res if with_replacement else list(dict.fromkeys(res))[:n]
+    if weights or with_replacement:
+        return random.choices(data, weights=weights, k=n)
     return random.sample(data, n)
