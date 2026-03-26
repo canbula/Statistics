2d46cd57102b26aba359a285cf77138058178c21
ENES DAĞ <mustafaenesdag@gmail.com>
2026-03-26 22:26:36 +0300

    Fix: 6 lines and keyword-only with_replacement

--- Week02/weighted_mustafaenes_dag.py
+++ Week02/weighted_mustafaenes_dag.py
@@ -1,8 +1,6 @@
 import random
 
-def weighted_srs(data, n, weights, with_replacement=False):
+def weighted_srs(data, n, weights, *, with_replacement=False):
     if with_replacement:
         return random.choices(data, weights=weights, k=n)
-    else:
-        return random.sample(data, k=n, counts=weights)
-# 
+    return random.sample(data, k=n, counts=weights)
