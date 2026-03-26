1f9e4cf2831d403f7513af3d894d554b084fc510
ENES DAĞ <mustafaenesdag@gmail.com>
2026-03-26 23:07:27 +0300

    Trigger tests

--- Week02/weighted_mustafaenes_dag.py
+++ Week02/weighted_mustafaenes_dag.py
@@ -4,3 +4,4 @@ def weighted_srs(data, n, weights, with_replacement=False):
     if with_replacement:
         return random.choices(data, weights=weights, k=n)
         return random.sample(data, k=n, counts=weights)
+# 
