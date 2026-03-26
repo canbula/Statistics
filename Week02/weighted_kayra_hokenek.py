1d7b7cb485ad98543eb11d4ad638318ca0051655
kayra-jpeg <ahmet123123123123ahmet@gmail.com>
2026-03-26 22:05:52 +0300

    Add files via upload

---
+++ Week02/weighted_kayra_hokenek.py
@@ -0,0 +1,7 @@
+import random
+
+def weighted_srs(data, n, weights, with_replacement = False):
+    if with_replacement or weights is not None:
+        return random.choices(data, weights = weights, k = n)
+    else:
+        return random.sample(data, n)
