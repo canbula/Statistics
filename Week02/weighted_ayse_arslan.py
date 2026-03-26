68796d7070a17c57124a862dc795cc61f70a8f24
Bora Canbula <bora.canbula@cbu.edu.tr>
2026-03-26 13:18:36 +0300

    Add comment to import statement in weighted_ayse_arslan.py
    Added a comment to the import statement for clarity.

--- Week02/weighted_ayse_arslan.py
+++ Week02/weighted_ayse_arslan.py
@@ -1,4 +1,4 @@
-import random
+import random  #
 def weighted_srs(data, n, weights, with_replacement=False):
     if weights==None:
         return random.choices(data,k=n) if with_replacement else random.sample(data,n)
