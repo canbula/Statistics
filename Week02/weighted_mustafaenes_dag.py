283ee38d1c6ace6eec276f6ef41dc831b285d82f
enesdag45 <mustafaenesdag@gmail.com>
2026-03-26 22:49:29 +0300

    Update weighted_mustafaenes_dag.py

--- Week02/weighted_mustafaenes_dag.py
+++ Week02/weighted_mustafaenes_dag.py
@@ -3,5 +3,4 @@ import random
 def weighted_srs(data, n, weights, with_replacement=False):
     if with_replacement:
         return random.choices(data, weights=weights, k=n)
-    else:
         return random.sample(data, k=n, counts=weights)
