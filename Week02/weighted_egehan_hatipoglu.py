3fcc2c0aab25ec732c591703040a8fa7b0a71432
Egehan Hatipoğlu <egehanhatipoglu344@gmail.com>
2026-03-26 17:14:41 +0300

    Refactor weighted_srs function for sampling logic

--- Week02/weighted_egehan_hatipoglu.py
+++ Week02/weighted_egehan_hatipoglu.py
@@ -1,10 +1,7 @@
 import random
-def weighted_srs(data, n, weights, with_replacement):
-    if with_replacement == True: return random.choices(data, weights=weights, k=n)
-    sample = []
-    copied_data, copied_weights = data.copy(), weights.copy()
-    for i in range(n):
-        index = random.choices(range(len(copied_data)), weights=copied_weights, k=1)[0]
-        sample.append(copied_data.pop(index))
-        copied_weights.pop(index)
-    return sample
+def weighted_srs(data, n, weights, with_replacement = False):
+    if with_replacement == True or weights != None:
+        sample = random.choices(data, k = n, weights = weights)
+    else:
+        sample = random.sample(data, n)
+    return sample 
