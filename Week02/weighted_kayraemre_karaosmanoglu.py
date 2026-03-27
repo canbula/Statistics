d4b4b55ecd8789d3fb4384948ea4c5e29d8ea07e
Shar0x <kayra27emre@gmail.com>
2026-03-27 10:00:27 +0300

    Update weighted_kayraemre_karaosmanoglu.py

--- Week02/weighted_kayraemre_karaosmanoglu.py
+++ Week02/weighted_kayraemre_karaosmanoglu.py
@@ -1,11 +1,9 @@
 import random
-
-def weighted_srs(data, n, weights, with_replacement=True):
-    if with_replacement: 
-        return random.choices(data, weights=weights, k=n)
-    d, w, res = list(data), list(weights), []
-    for _ in range(n):
-        i = random.choices(range(len(d)), weights=w)[0]
-        res.append(d.pop(i)); w.pop(i)
+def weighted_srs(data, n, weights, with_replacement=False):
+    w = weights if weights else [1]*len(data)
+    if with_replacement or weights:
+        return random.choices(data, weights=w, k=n)
+    res, d_c = [], list(data)
+    for _ in range(n): res.append(d_c.pop(random.randrange(len(d_c))))
     return res
   
