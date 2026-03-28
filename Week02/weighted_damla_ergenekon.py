335d390c8e2b8abb4f6cae3cf210a2c77846828c
DamlaErgenekon <2541001912@ogr.cbu.edu.tr>
2026-03-28 23:54:14 +0300

    Update weighted_damla_ergenekon.py

--- Week02/weighted_damla_ergenekon.py
+++ Week02/weighted_damla_ergenekon.py
@@ -1,7 +1,6 @@
-import random  #
-def weighted_srs(data, n, weights, with_replacement):
-    if with_replacement:
-        return random.choices(data, weights=weights, k=n)
+import random
+def weighted_srs(data, n, weights, *, with_replacement=False):
+    if with_replacement: return random.choices(data, weights=weights, k=n)
     d, w, sample = list(data), list(weights), []
     for _ in range(n):
         idx = random.choices(range(len(d)), weights=w, k=1)[0]
