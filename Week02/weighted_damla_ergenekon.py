928bf96d8097c6ec21c31e9ad442581e00f30031
DamlaErgenekon <2541001912@ogr.cbu.edu.tr>
2026-03-29 00:24:32 +0300

    Update weighted_damla_ergenekon.py

--- Week02/weighted_damla_ergenekon.py
+++ Week02/weighted_damla_ergenekon.py
@@ -1,8 +1,9 @@
 import random
-
 def weighted_srs(data, n, weights, with_replacement=False):
-    if weights is None and with_replacement is False:
-        sample_list = random.sample(data, n)
-    else:
-        sample_list = random.choices(data, weights=weights, k=n)
-    return sample_list
+    if with_replacement: return random.choices(data, weights=weights, k=n)
+    if not weights: return random.sample(data, n)
+    indexes = []
+    while len(indexes) < n:
+        i = random.choices(range(len(data)), weights=weights)[0]
+        if i not in indexes: indexes.append(i)
+    return [data[i] for i in indexes]
