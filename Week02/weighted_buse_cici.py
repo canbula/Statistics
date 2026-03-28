18400b76ad267214811ec3c28995a062b70148b2
Biyse <mrs.busecici@gmail.com>
2026-03-28 19:07:25 +0300

    Refactor weighted_srs function for clarity

--- Week02/weighted_buse_cici.py
+++ Week02/weighted_buse_cici.py
@@ -1,8 +1,9 @@
 import random
-def weighted_srs(data, n, weights, *, with_replacement=True):
+def weighted_srs(data, n, weights, *, with_replacement):
+    if not with_replacement and n > len(data): return None
     sample, d, w = [], list(data), list(weights)
     for _ in range(n):
-        choise = random.choices(d, weights=w, k=1)[0]
-        sample.append(choise)
-        if not with_replacement: ind = d.index(choise); d.pop(ind); w.pop(ind)
+        c = random.choices(d, weights=w, k=1)[0]; sample.append(c)
+        if not with_replacement:
+            i = d.index(c); d.pop(i); w.pop(i)
     return sample
