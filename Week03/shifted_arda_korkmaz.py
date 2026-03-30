a65f5ff1cafcbf09f0a1310bec39360ea5e6fbd0
Arda Korkmaz <143834039+Ardakorkmaz0@users.noreply.github.com>
2026-03-30 11:43:46 +0300

    Refactor shifted function for median calculation

--- Week03/shifted_arda_korkmaz.py
+++ Week03/shifted_arda_korkmaz.py
@@ -1,12 +1,9 @@
 def shifted(data):
-    if not data: return 0.0
     n = len(data)
+    if n == 0: return 0.0
     mean = sum(data) / n
     s = sorted(data)
     mid = n // 2
-    if n % 2 == 0:
-        median = (s[mid - 1] + s[mid]) / 2
-    else:
-        median = s[mid]
-    if median == 0: return 0.0
-    return ((mean - median) / median) * 100
+    res = (s[mid-1] + s[mid]) / 2 if n % 2 == 0 else s[mid]
+    if res == 0: return 0.0
+    return abs((mean - res) / res) * 100
