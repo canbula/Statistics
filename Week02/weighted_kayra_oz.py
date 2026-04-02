b6ff124831346a709070114cc5880c5c890f59f9
1kayraoz <244101021@ogr.cbu.edu.tr>
2026-04-02 20:44:30 +0300

    Update weighted_kayra_oz.py

--- Week02/weighted_kayra_oz.py
+++ Week02/weighted_kayra_oz.py
@@ -1,10 +1,9 @@
 import random
 
-def weighted_srs(data, n, weights, with_replacement):
-    if with_replacement: return random.choices(data, weights=weights, k=n)
-    d, w, res = list(data), list(weights), []
-    for _ in range(n):
-        idx = random.choices(range(len(d)), weights=w)[0]
-        res.append(d.pop(idx))
-        w.pop(idx)
-    return res
+def weighted_srs(data, n, weights, with_replacement=False):
+    if weights is None:
+        return random.choices(data, k=n) if with_replacement else random.sample(data, n)
+    if with_replacement:
+        return random.choices(data, weights=weights, k=n)
+    population = sum([[d]*w for d, w in zip(data, weights)], [])
+    return random.sample(population, n)
