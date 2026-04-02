53526a42acfdbdb3685a10bffbabf3dc69ddf1a4
Pelin <akkuspelin029@gmail.com>
2026-04-02 21:45:17 +0300

    Update weighted_pelin_akkus.py

--- Week02/weighted_pelin_akkus.py
+++ Week02/weighted_pelin_akkus.py
@@ -1,11 +1,9 @@
 import random
 
-def weighted_srs(data,n,weights,with_replacement):
-    d,w,r=data[:],weights[:],[]   # copies the list so original data is not modified.
-    for _ in range(n):
-        x=random.uniform(0,sum(w)); s=0
-        for i,v in enumerate(w):
-            s+=v
-            if s>=x: r.append(d[i]); break   # choose item where random value falls.
-        if not with_replacement: d.pop(i); w.pop(i)  # remove item if no replacement.
-    return r
+def weighted_srs(data, n, weights, with_replacement=False):
+    if weights is None:
+        return random.choices(data, k=n) if with_replacement else random.sample(data, n)
+    if with_replacement:
+        return random.choices(data, weights=weights, k=n)
+    population = sum([[d]*w for d, w in zip(data, weights)], [])
+    return random.sample(population, n)
