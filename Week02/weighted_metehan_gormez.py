f7b18ad3f3ac562a8f6f326df188ae1d616c4c33
metehangormez <gormezsmetehan@gmail.com>
2026-04-03 10:08:24 +0300

    Refactor weighted sampling logic in weighted_srs function

--- Week02/weighted_metehan_gormez.py
+++ Week02/weighted_metehan_gormez.py
@@ -1,8 +1,7 @@
 import random
 
 def weighted_srs(data, n, weights, with_replacement=False):
-    if weights and with_replacement:
+    if with_replacement:
         return random.choices(data, weights=weights, k=n)
-    elif weights:
-        return random.sample(data, counts=[int(w*100) for w in weights], k=n)
-    return random.choices(data, k=n) if with_replacement else random.sample(data, n)
+    counts = [int(w * 10000) + 1 for w in weights] if weights else None
+    return random.sample(data, counts=counts, k=n) if weights else random.sample(data, n)
