ae2c4d9fe8a3dd3dc0a96ff21526036efb373feb
Kerem Çağan Ülver <kerulver@hotmail.com>
2026-04-03 09:35:25 +0300

    Refactor weighted_srs for improved sampling with weights
    Refactor weighted_srs function to handle weights more effectively and remove sampling without replacement.

--- Week02/weighted_keremcagan_ulver.py
+++ Week02/weighted_keremcagan_ulver.py
@@ -1,6 +1,9 @@
 import random
 
-def weighted_srs(data, n, weights=None, with_replacement=False):
-    if with_replacement: return random.choices(data, k=n)
-    if weights is None: return random.sample(data, k=n)
-    return random.choices(data, weights=weights, k=n)
+def weighted_srs(data, n, weights, with_replacement=False):
+    if with_replacement: return random.choices(data, weights=weights, k=n)
+    res, temp_w = [], list(weights)
+    for _ in range(n):
+        i = random.choices(range(len(data)), weights=temp_w)[0]
+        res.append(data[i]); temp_w[i] = 0
+    return res
