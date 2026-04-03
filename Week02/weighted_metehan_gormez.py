601b58a84460c36ebcae23a971274593de556e28
metehangormez <gormezsmetehan@gmail.com>
2026-04-03 10:05:42 +0300

    Refactor weighted_srs function for better weight handling

--- Week02/weighted_metehan_gormez.py
+++ Week02/weighted_metehan_gormez.py
@@ -1,6 +1,8 @@
 import random
 
 def weighted_srs(data, n, weights, with_replacement=False):
-    if with_replacement:
+    if weights and with_replacement:
         return random.choices(data, weights=weights, k=n)
-    return random.sample(data, counts=[int(w*100) for w in weights], k=n)
+    elif weights:
+        return random.sample(data, counts=[int(w*100) for w in weights], k=n)
+    return random.choices(data, k=n) if with_replacement else random.sample(data, n)
