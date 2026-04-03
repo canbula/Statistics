dc7bb157a19ec40079a84572ee38c2592a8d063f
metehangormez <gormezsmetehan@gmail.com>
2026-04-03 09:53:39 +0300

    Update and rename weighted_metehan_gormez to weighted_metehan_gormez.py

---
+++ Week02/weighted_metehan_gormez.py
@@ -0,0 +1,8 @@
+import random
+
+def weighted_srs(data, n, weights, with_replacement = False):
+    if not (with_replacement or weights):
+        sample = random.sample(data, n)
+    else:
+        sample = random.choices(data, weights=weights, k=n)
+    return sample
