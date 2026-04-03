097e8ee5ec20ea8022861abb6fdb094455a0f4e3
metehangormez <gormezsmetehan@gmail.com>
2026-04-03 10:04:05 +0300

    Modify sampling method to use weighted counts

--- Week02/weighted_metehan_gormez.py
+++ Week02/weighted_metehan_gormez.py
@@ -3,4 +3,4 @@ import random
 def weighted_srs(data, n, weights, with_replacement=False):
     if with_replacement:
         return random.choices(data, weights=weights, k=n)
-    return random.sample(data, n)
+    return random.sample(data, counts=[int(w*100) for w in weights], k=n)
