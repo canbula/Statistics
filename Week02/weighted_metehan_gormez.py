fef55936440f907f414e8793494b225e87607f16
metehangormez <gormezsmetehan@gmail.com>
2026-04-03 10:02:25 +0300

    Refactor weighted_srs function for simplicity

--- Week02/weighted_metehan_gormez.py
+++ Week02/weighted_metehan_gormez.py
@@ -1,13 +1,6 @@
 import random
 
-def weighted_srs(data, n, weights=None, with_replacement=False):
-    if weights is None:
-        if with_replacement:
-            return random.choices(data, k=n)
-        else:
-            return random.sample(data, n)
-    else:
-        if with_replacement:
-            return random.choices(data, weights=weights, k=n)
-        else: 
-            return random.choices(data, weights=weights, k=n)
+def weighted_srs(data, n, weights, with_replacement=False):
+    if with_replacement:
+        return random.choices(data, weights=weights, k=n)
+    return random.sample(data, n)
